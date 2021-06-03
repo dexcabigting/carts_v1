b454391c38ad1b3e15c817dff51f2ae6f8a583ca
dexcabigting <dexcabigting1234@gmail.com>
2021-06-03 17:37:08 +0800

    Add update password in profile update page

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -10,10 +10,10 @@
             
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
 
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6 bg-white border-b border-gray-200 divide-y divide-gray-300">
 
-                    <div>
-                        <h2 class="font-semibold text-xl text-gray-800 leading-tight mb-4">
+                    <div class="pb-4">
+                        <h2 class="font-semibold text-l text-gray-800 leading-tight mb-4">
                             {{ __('Credentials') }}
                         </h2>
 
@@ -37,6 +37,47 @@
                             <div class="flex items-center justify-end mt-4">
                                 <x-button>
                                     {{ __('Update Credentials') }}
+                                </x-button>
+                            </div>
+
+                        </form>
+                    </div>
+
+                    <div class="pt-4">
+                        <h2 class="font-semibold text-l text-gray-800 leading-tight mb-4">
+                            {{ __('Password') }}
+                        </h2>
+
+                        <form method="POST" action="{{ route('profile.update-credentials') }}">
+                            @method('PUT')
+                            @csrf
+
+                            <div class="mt-4">
+                                <x-label for="password" :value="__('Current Password')" />
+                                <x-input id="password" class="block mt-1 w-full"
+                                                type="password"
+                                                name="password"
+                                                required />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-label for="new_password" :value="__('New Password')" />
+                                <x-input id="new_password" class="block mt-1 w-full"
+                                                type="password"
+                                                name="new_password"
+                                                required />
+                            </div>
+
+                            <div class="mt-4">
+                                <x-label for="new_password_confirmation" :value="__('Confirm New Password')" />
+                                <x-input id="new_password_confirmation" class="block mt-1 w-full"
+                                                type="password"
+                                                name="new_password_confirmation" required />
+                            </div>
+
+                            <div class="flex items-center justify-end mt-4">
+                                <x-button>
+                                    {{ __('Update Password') }}
                                 </x-button>
                             </div>
 
