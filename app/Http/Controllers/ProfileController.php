844d1e513244c199d5b9cc49a9b9750d3181ac1b
dexcabigting <dexcabigting1234@gmail.com>
2021-06-03 12:50:15 +0800

    Implement profile update with success and error message

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
-    public function update( $request)
+    public function update(ProfileUpdateRequest $request)
     {
+        auth()->user()->update($request->only('name', 'email'));
 
+        if ($request->input('password')) {
+            auth()->user()->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+
+        return redirect()->route('profile.index')->with('message', 'Profile updated successfully!');
     } 
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -12,34 +12,33 @@
 
                 <div class="p-6 bg-white border-b border-gray-200">
 
+                    <x-success-message class="mb-4" />
+                    <x-validation-errors class="mb-4" :errors="$errors" />
+
                     <form method="POST" action="{{ route('profile.update') }}">
                         @method('PUT')
                         @csrf
 
                         <div>
                             <x-label for="name" :value="__('Name')"/>
-
-                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="{{ Auth::user()->name }}" :value="old('name')" required autofocus />
+                            <x-input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ Auth::user()->name }}" autofocus required />
                         </div>
 
                         <div class="mt-4">
                             <x-label for="email" :value="__('Email')" />
-
-                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" placeholder="{{ Auth::user()->email }}" :value="old('email')" required />
+                            <x-input id="email" class="block mt-1 w-full" type="email" name="email" value="{{ Auth::user()->email }}" required />
                         </div>
 
                         <div class="mt-4">
-                            <x-label for="password" :value="__('Password')" />
-
-                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+                            <x-label for="password" :value="__('New Password')" />
+                            <x-input id="password" class="block mt-1 w-full" type="password" name="password" />
                         </div>
 
                         <div class="mt-4">
                             <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
                             <x-input id="password_confirmation" class="block mt-1 w-full"
                                                 type="password"
-                                                name="password_confirmation" required />
+                                                name="password_confirmation" />
                         </div>
 
                         <div class="flex items-center justify-end mt-4">
