3a5a11ce6319a504d3eb2f09bc71e1fd044f7db6
dexcabigting <dexcabigting1234@gmail.com>
2021-06-26 15:59:00 +0800

    Modified admin middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class admin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (auth()->user()->role == 'admin') {
+        if (auth()->user()->role_id == 1) {
             return $next($request);
         }
         abort(403);
