3e5f342f091d71a195d9fc401d095d4f0e15d053
dexcabigting <dexcabigting1234@gmail.com>
2021-09-22 20:23:55 +0800

    Created product factory and seeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'prd_name' => $this->faker->words(),
+            'prd_description' => $this->faker->text(50),
+            'prd_price' => randomFloat($nbMaxDecimals = NULL, $min = 750.00, $max = 1000.00),
+        ];
+    }
+}
