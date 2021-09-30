9428036985946ab4fe30eaf4c093e1cea35ef4bd
dexcabigting <dexcabigting1234@gmail.com>
2021-09-30 15:39:50 +0800

    Created carts create component

---
+++ app/Http/Livewire/Shop/Carts/CartsCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Shop\Carts;
+
+use Livewire\Component;
+
+class CartsCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.shop.carts.carts-create');
+    }
+}
