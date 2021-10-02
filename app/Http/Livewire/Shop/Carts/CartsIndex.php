e49b7791cc459305ee9416df66ee6b183e3ae0b8
dexcabigting <dexcabigting1234@gmail.com>
2021-10-02 23:01:41 +0800

    Created carts index data table

--- app/Http/Livewire/Shop/Carts/CartsIndex.php
+++ app/Http/Livewire/Shop/Carts/CartsIndex.php
@@ -4,18 +4,52 @@ namespace App\Http\Livewire\Shop\Carts;
 
 use Livewire\Component;
 use App\Models\User;
+use Livewire\WithPagination;
 
 class CartsIndex extends Component
 {
-    public $userCarts = [];
+    use WithPagination;
+
+    public $cartId;
+    public $cartModal = false;
+
+    protected $listeners = [
+        'closeEditCartModal',
+    ];
 
     public function mount()
     {
-        $this->userCarts = auth()->user()->carts;
+        
     }
 
     public function render()
     {
-        return view('livewire.shop.carts.carts-index');
+        $userCarts = $this->user_carts->paginate(6);
+
+        $totalAmount = $this->total_amount;
+
+        return view('livewire.shop.carts.carts-index', compact('userCarts', 'totalAmount'));
+    }
+
+    public function getUserCartsProperty()
+    {
+        return auth()->user()->carts();
+    }
+
+    public function getTotalAmountProperty()
+    {
+        return $this->user_carts->sum('subtotal');
+    }
+
+    public function openEditCartModal($id)
+    {
+        $this->cartId = $id;
+
+        $this->cartModal = true;
+    }
+
+    public function openDeleteCartModal($id)
+    {
+        $this->cartId = $id;
     }
 }
