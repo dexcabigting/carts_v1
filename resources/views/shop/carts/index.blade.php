da72a0a76135c2ba7b7e3089f9dd49e0e6550a81
dexcabigting <dexcabigting1234@gmail.com>
2021-10-02 18:43:16 +0800

    Fixed carts index grid

--- resources/views/shop/carts/index.blade.php
+++ resources/views/shop/carts/index.blade.php
@@ -10,7 +10,7 @@
     </div>
 
     <div class="pb-6">
-        <div class="grid grid-cols-2 gap-6 max-w-6xl mx-auto">
+        <div class="grid grid-cols-2 auto-cols-auto gap-6 max-w-6xl mx-auto">
             <div>
                 @forelse ($userCarts as $userCart)
                 <div class="pb-6">
@@ -29,9 +29,11 @@
                 @endforelse
             </div>
 
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 overflow-x-auto">
-                    Hello
+            <div>
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200 overflow-x-auto">
+                        Hello
+                    </div>
                 </div>
             </div>
         </div>
