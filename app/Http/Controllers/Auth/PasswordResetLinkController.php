a19b419962abccbf2216af8f666ce4fbbec3d7ee
dexcabigting <dexcabigting1234@gmail.com>
2021-10-04 12:34:24 +0800

    Updated password reset link

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -83,42 +83,40 @@ class PasswordResetLinkController extends Controller
 
         $user->save();
 
-        $user_email = $user->email;
-
         $token = Str::random(30);
 
         session(['token' => $token]) ;
 
         $shown_token = sha1($token.$user->id.$token);
 
+        $to = $user->phone;
+        $from = getenv("TWILIO_FROM");
+        $message = $user->otp;
+        //open connection
+
+        $ch = curl_init();
+
+        //set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_USERPWD, getenv("TWILIO_SID").':'.getenv("TWILIO_TOKEN"));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+        curl_setopt($ch, CURLOPT_URL, sprintf('https://api.twilio.com/2010-04-01/Accounts/'.getenv("TWILIO_SID").'/Messages.json', getenv("TWILIO_SID")));
+        curl_setopt($ch, CURLOPT_POST, 3);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'To='.$to.'&From='.$from.'&Body='.$message);
+
+        // execute post
+        $result = curl_exec($ch);
+        $result = json_decode($result);
+
+        // close connection
+        curl_close($ch);
+        //Sending message ends here
+
         return redirect()->route('password.reset-phone', [$user, 'token' => $shown_token]);
 
         // dd($user_email, $user['otp'], $request->_token);
 
         // // Open connection
-        // $to = $phone;
-        // $from = getenv("TWILIO_FROM");
-        // $message = mt_rand(1000, 9999);
-
-
-        // $ch = curl_init();
-
-        // // Set the url, number of POST vars, POST data
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        // curl_setopt($ch, CURLOPT_USERPWD, getenv("TWILIO_SID").':'.getenv("TWILIO_TOKEN"));
-        // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-        // curl_setopt($ch, CURLOPT_URL, sprintf('https://api.twilio.com/2010-04-01/Accounts/'.getenv("TWILIO_SID").'/Messages.json', getenv("TWILIO_SID")));
-        // curl_setopt($ch, CURLOPT_POST, 3);
-        // curl_setopt($ch, CURLOPT_POSTFIELDS, 'To='.$to.'&From='.$from.'&Body='.$message);
-
-        // // Execute post
-        // $result = curl_exec($ch);
-        // $result = json_decode($result);
-
-        // // Close connection
-        // curl_close($ch);
-        // //Sending message ends here
-
-        // return [$result];
+        
     }
 }
