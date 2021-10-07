5c9c9b265a2261efa6c36f9e8a799e57e7ed56a0
dexcabigting <dexcabigting1234@gmail.com>
2021-10-07 18:43:49 +0800

    Added CategorySeeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories = [
+            [
+                'ctgr_name' => 'Jersey',
+                'ctgr_description' => 'Basketball uniforms consist of a jersey that features the number and last name of the player on the back, as well as shorts.',
+            ],
+            [
+                'ctgr_name' => 'T-Shirt',
+                'ctgr_description' => 'T-Shirt is a style of fabric shirt named after the T shape of its body and sleeves.',
+            ],
+        ];
+
+        Category::insert($categories);
+    }
+}
