96127af34ce0276aaaf000fa2c463385b4e01579
dexcabigting <dexcabigting1234@gmail.com>
2021-10-09 14:52:34 +0800

    Added Category and Fabric on DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             UserSeeder::class,
-            // ProductSeeder::class,
+            Category::class,
+            Fabric::class,
         ]);
     }
 }
