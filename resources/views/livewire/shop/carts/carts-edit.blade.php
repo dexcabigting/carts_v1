fd0eacf7b4ac4a29d1602f1bf56f28e97d0cfa2c
dexcabigting <dexcabigting1234@gmail.com>
2021-10-15 22:17:12 +0800

    Renamed edit-1 to edit

--- resources/views/livewire/shop/carts/carts-edit.blade.php
+++ resources/views/livewire/shop/carts/carts-edit.blade.php
@@ -7,7 +7,7 @@
         <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white shadow-sm rounded-lg border-2 border-transparent">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @include('shop.carts.edit-1')
+                    @include('shop.carts.edit')
                 </div>
             </div>
         </div>
