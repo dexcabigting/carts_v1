0a59a38108a2c2d929083c4de029feef1f511cae
dexcabigting <dexcabigting1234@gmail.com>
2021-10-17 13:52:19 +0800

    Generated CartsDelete livewire component

---
+++ app/Http/Livewire/Shop/Carts/CartsDelete.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Shop\Carts;
+
+use Livewire\Component;
+
+class CartsDelete extends Component
+{
+    public function render()
+    {
+        return view('livewire.shop.carts.carts-delete');
+    }
+}
