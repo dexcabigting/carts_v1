613d252f9e0dd64ff9101f635b75fd4eeb14e081
dexcabigting <dexcabigting1234@gmail.com>
2021-10-17 14:49:19 +0800

    Completed CartsDelete component

--- app/Http/Livewire/Shop/Carts/CartsDelete.php
+++ app/Http/Livewire/Shop/Carts/CartsDelete.php
@@ -3,11 +3,52 @@
 namespace App\Http\Livewire\Shop\Carts;
 
 use Livewire\Component;
+use App\Models\Cart;
 
 class CartsDelete extends Component
 {
+    public $carts = [];
+
+    public $promptDelete = 1;
+    public $promptDeleted = 0;
+
+    public function mount(...$id)
+    {
+        $this->carts = collect($id)->flatten()->toArray();
+    }
+
     public function render()
     {
         return view('livewire.shop.carts.carts-delete');
+    }
+
+    public function deleteCarts()
+    {
+        if (count($this->carts) == 1) {
+            $flash = 'Cart has been deleted successfully!';
+        } else {
+            $flash = 'Carts have been deleted successfully!';
+        }
+
+        Cart::whereIn('id', $this->carts)->delete();
+
+        $this->emitUp('unsetCheckedCarts', $this->carts);
+
+        $this->emitUp('cleanse');
+
+        $this->emitUp('refreshParent');
+
+        session()->flash('success', $flash);
+
+        $this->carts = [];
+
+        $this->promptDelete = 0;
+        $this->promptDeleted = 1;
+    }
+
+    public function closeDeleteCartModal()
+    {
+        $this->dispatchBrowserEvent('cartDeleteModalDisplayNone');
+        $this->emitUp('closeDeleteCartModal');
     }
 }
--- resources/views/livewire/shop/carts/carts-delete.blade.php
+++ resources/views/livewire/shop/carts/carts-delete.blade.php
@@ -1,3 +1,21 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div id="cart-delete-modal" class="z-20 flex flex-wrap flex-row justify-items-center align-items-center absolute top-0 left-0 w-full h-full">
+    <div class="py-12 fixed top-0 left-0 w-full h-full bg-gray-500 opacity-75">
+
+    </div>
+
+    <div class="m-auto z-30">
+        <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white shadow-sm rounded-lg border-2 border-transparent">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    @include('shop.carts.delete')
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script>
+    window.addEventListener('cartDeleteModalDisplayNone', event => {
+        document.getElementById("cart-delete-modal").style.display = "none";
+    });
+</script>
