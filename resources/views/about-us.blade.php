7d6743b91f5330e27fb85de72bfb68c14f1cc2b1
Lance Manalese <mnlslance69@gmail.com>
2021-11-17 14:14:39 -0800

    about us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -1,5 +1,50 @@
 <x-guest-layout class="overflow-x-hidden">
-    <h1 class="text-2xl text-white">
-        Lagay mo rito
-    </h1>
+         <div class="relative flex justify-center items-center md:-mt-24 py-24">
+    <h1 class="absolute md:text-2xl top-36 md:top-48 text-gray-100 text-center w-1/2">EJ EZON SPORTSWEAR IS A CLOTHING LINE MAINLY
+      FOCUS ON
+      CREATING <span class="underline">QUALITY</span> BASKETBALL JERSEY. </h1>
+    <h1 class="text-7xl md:text-9xl text-custom-text font-extrabold  top-2">ABOUT</h1>
+        </div>
+  <div class="lg:flex-row flex-col flex justify-center items-center">
+    <img class="hidden sm:block sm:w-0 lg:ml-32 lg:w-1/4 2xl:ml-64" src="img/Group 5.svg" alt="">
+    <div class="flex justify-center items-center">
+      <div class="flex flex-col mx-12 md:mx-32">
+        <!-- purple bar -->
+        <img class="w-10 h-10" src="img/Rectangle 6.svg" alt="">
+        <h1 class="text-gray-100 text-2xl md:text-4xl font-semibold my-2 lg:text-2xl">What's inside?</h1>
+        <p class="text-gray-100 md:text-2xl md:w-2/3 my-4 lg:text-base">The best of the best quality precision crafted
+          by the experts for professional basketball players.</p>
+        <div class="flex flex-col md:mx-8">
+
+          <div class="flex-row flex my-6">
+            <img class="w-14 h-14 md:w-24 md:h-24 lg:h-10 lg:w-10 2xl:w-14 2xl:h-14" src="img/mico.svg" alt="">
+            <div class="flex flex-col my-1 mx-7">
+              <h1 class="font-semibold text-2xl text-gray-100">Microfiber</h1>
+              <p class="w-full text-md text-gray-100">as the name suggests, is a material
+                made of fine tiny thread fibers with a linear
+                density of not more than one denier. </p>
+            </div>
+          </div>
+
+          <div class="flex-row flex my-6">
+            <img class="w-14 h-14 md:w-24 md:h-24 lg:h-10 lg:w-10 2xl:w-14 2xl:h-14" src="img/poly.svg" alt="">
+            <div class="flex flex-col my-1 mx-7">
+              <h1 class="font-semibold text-2xl text-gray-100">Polyester</h1>
+              <p class="w-full text-md text-gray-100">A cloth made out of plastic fibers making it light-weight,
+                wrinkle-free, long lasting and breathable. </p>
+            </div>
+          </div>
+
+          <div class="flex-row flex my-6">
+            <img class="w-14 h-14 md:w-24 md:h-24  lg:h-10 lg:w-10 2xl:w-14 2xl:h-14" src="img/x.svg" alt="">
+            <div class="flex flex-col my-1 mx-7">
+              <h1 class="font-semibold text-2xl text-gray-100">X- Static</h1>
+              <p class="w-full text-md text-gray-100">Silver is a metal with anti-bacterial properties that prevents
+                the accumulation of bacteria and fungus. X-Static sportswear is,
+                therefore, clean and free of odor even after excessive use.</p>
+            </div>
+
+          </div>
+
+       
 </x-guest-layout>
