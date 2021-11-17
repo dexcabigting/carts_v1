8f6d8c39eed98d3322a0199159f655adede7d006
dexcabigting <dexcabigting1234@gmail.com>
2021-11-17 21:17:36 +0800

    Fixed guess nav links

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -53,6 +53,7 @@
                 <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
               </a>
 
+              @guest
               <a href="{{ route('login') }}" class="px-2 py-4 text-white bg-custom-blacki flex justify-between w-full border-b border-custom-text hover:text-gray-200 hover:bg-custom-violet">
                 LOGIN
                 <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
@@ -62,6 +63,7 @@
                 REGISTER
                 <svg class="w-6 h-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M9 5l7 7-7 7"></path></svg>    
               </a>
+              @endguest
           </nav>
         </div>
       </div>
@@ -72,13 +74,32 @@
         
       <img class="sm:block py-2 lg:px-12 sm:h-20 sm:w-10 h-10 w-40 -mt-24 md:h-auto md:w-auto sm:mt-2 md:mt-0 sm:mx-8" src="img/Group 12.svg" />      
 
-      <h1 class="hidden sm:block py-4 lg:px-12 md:px-2 sm:px-5">
+      @guest
+        <h1 class="hidden sm:block py-4 lg:px-12 md:px-2 sm:px-5">
         <a class="hidden sm:block" href="{{ route('login') }}">LOGIN</a>
-      </h1>
+        </h1>
 
-      <h1 class="hidden sm:block py-4 lg:px-12 px-2 sm:px-5">
-        <a href="{{ route('register') }}">REGISTER</a>
-      </h1>      
+        <h1 class="hidden sm:block py-4 lg:px-12 px-2 sm:px-5">
+          <a href="{{ route('register') }}">REGISTER</a>
+        </h1>      
+      @endguest
+
+      @auth
+        @if(auth()->user()->role_id == 1)
+          <a  href="{{ route('dashboard') }}" class="hidden sm:block  py-4 lg:px-10 px-2 sm:px-5">DASHBOARD</a>
+        @else
+          <a  href="{{ route('shop.index') }}" class="hidden sm:block  py-4 lg:px-10 px-2 sm:px-5">SHOP NOW</a>
+        @endif
+
+        <h1 class="hidden sm:block py-4 lg:px-12 px-2 sm:px-5">
+          <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">
+              LOGOUT 
+            </button>
+          </form>
+        </h1>
+      @endauth
     </header>
 
     <div>
