b8c45de923091dbf9054c69afba1a521ed6dd2cd
dexcabigting <dexcabigting1234@gmail.com>
2021-11-18 13:56:01 +0800

    FabricsCreate component is now working

---
+++ app/Http/Livewire/Fabrics/FabricsCreate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Livewire\Fabrics;
+
+use Livewire\Component;
+use App\Models\Fabric;
+
+class FabricsCreate extends Component
+{
+    public $form = [
+        'fab_name' => '',
+        'fab_description' => '',
+    ];
+
+    protected $rules = [
+        'form.fab_name' => 'required|string|max:255',
+        'form.fab_description' => 'required|string|max:255',
+    ];
+
+    protected $validationAttributes = [
+        'form.fab_name' => 'fabric name',
+        'form.fab_description' => 'fabric description',
+    ];
+
+    public function render()
+    {
+        return view('livewire.fabrics.fabrics-create');
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        Fabric::create([
+            'fab_name' => $this->form['fab_name'],
+            'fab_description' => $this->form['fab_description'],
+        ]);
+
+        $this->clearFormFields();
+
+        $this->emitUp('refreshParent');
+
+        session()->flash('success', 'Fabric has been created successfully!'); 
+    }
+
+    public function clearFormFields()
+    {
+        $this->form = [
+            'fab_name' => '',
+            'fab_description' => '',
+        ];
+    }
+
+    public function closeCreateModal()
+    {
+        $this->dispatchBrowserEvent('createModalDisplayNone');
+        $this->emitUp('closeCreateModal');
+    }
+}
---
+++ resources/views/livewire/fabrics/fabrics-create.blade.php
@@ -0,0 +1,21 @@
+<div id="create-modal" class="z-10 flex flex-wrap flex-row justify-items-center align-items-center fixed top-0 left-0 w-full h-full">
+    <div class="fixed top-0 left-0 w-full h-full bg-gray-500 opacity-75">
+
+    </div>
+
+    <div class="p-5 m-auto z-20">
+        <div class="text-white mx-auto">
+            <div class="bg-white shadow-sm rounded-lg border-4 border-gray-500">
+                <div class="bg-custom-blacki border-b border-gray-200">
+                    @include('fabrics.create')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    window.addEventListener('createModalDisplayNone', event => {
+        document.getElementById("create-modal").style.display = "none";
+    });
+</script>
