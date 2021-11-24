1f9c94ae872133f1ac5eb5cab8baba463b0ae0ce
dexcabigting <dexcabigting1234@gmail.com>
2021-11-24 17:06:24 +0800

    Search and order by now working on OrdersIndex component

--- app/Http/Livewire/Orders/OrdersIndex.php
+++ app/Http/Livewire/Orders/OrdersIndex.php
@@ -4,15 +4,27 @@ namespace App\Http\Livewire\Orders;
 
 use Livewire\Component;
 use App\Models\Order;
+use Livewire\WithPagination;
 
 class OrdersIndex extends Component
 {
+    use WithPagination;
+
     public $viewModal = false;
     public $orderId;
+    public $search;
+    public $sortColumn;
+    public $sortDirection;
 
     protected $listeners = [
         'closeViewModal',
     ];
+
+    public function mount()
+    {
+        $this->sortColumn = 'invoice_number';
+        $this->sortDirection = 'asc';
+    }
 
     public function render()
     {
@@ -33,13 +45,19 @@ class OrdersIndex extends Component
     public function getOrdersProperty()
     {
         if(auth()->user()->role_id == 1) {
-            return Order::with(['user:id,name', 
-                                'order_variants:id,order_id,amount,product_variant_id', 
+            $search = '%' . $this->search . '%';
+            $sortColumn = $this->sortColumn;
+            $sortDirection = $this->sortDirection;
+
+            return Order::with(['order_variants:id,order_id,amount,product_variant_id', 
                                 'order_variants.product_variant' => function ($query) {
                                     $query->select('id', 'product_id', 'prd_var_name')
                                     ->with(['product:id,prd_name']);
                             }])
-                            ->withCount('order_items');  
+                            ->join('users', 'orders.user_id', '=', 'users.id')
+                            ->withCount('order_items')
+                            ->where('name', 'like', $search)
+                            ->orderBy($sortColumn, $sortDirection);
         } else {
             return Order::with(['order_variants:id,order_id,amount,product_variant_id', 
                                 'order_variants.product_variant' => function ($query) {
@@ -49,6 +67,11 @@ class OrdersIndex extends Component
                             ->where('user_id', auth()->user()->id)
                             ->withCount('order_items');              
         }
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
     }
 
     public function openViewModal($id)
--- resources/views/orders/index-admin.blade.php
+++ resources/views/orders/index-admin.blade.php
@@ -2,8 +2,49 @@
     <div class="pt-12 pb-6">
         <div class="max-w-5xl mx-auto">
             <div class="bg-custom-blacki overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-custom-blacki shadow-2xl text-6xl font-extrabold text-center text-gray-300 font-extrabold overflow-x-auto">
-                    My Orders
+                <div class="flex flex-row gap-5 p-6 bg-custom-blacki shadow-2xl overflow-x-auto">
+                     <!-- Order By -->
+                    <div class="px-4">
+                        <div class="flex flex-col xl:flex-row text-sm font-medium text-gray-100 py-4">
+                            <div class="w-1/3">
+                                <x-label :value="__('Order by')" class="font-semibold text-gray-50 inline-block text-base 2xl:text-xl" />
+                            </div>
+
+                            <div class="flex flex-row">
+                                <select wire:model="sortColumn" class="text-sm font-medium bg-custom-black text-white rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                                    <option value="name">
+                                        <x-label :value="__('Customer Name')" class="inline-block" />
+                                    </option>
+
+                                    <option value="invoice_number">
+                                        <x-label :value="__('Invoice Number')" class="inline-block" />
+                                    </option>
+
+                                    <option value="created_at">
+                                        <x-label :value="__('Date Created')" class="inline-block" />
+                                    </option>
+                                </select>
+
+                                <select wire:model="sortDirection" class="text-sm font-medium bg-custom-black text-white rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'">
+                                    <option value="asc">
+                                        <x-label :value="__('Ascending')" class="inline-block" />
+                                    </option>
+
+                                    <option value="desc">
+                                        <x-label :value="__('Descending')" class="inline-block" />
+                                    </option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Search Bar -->
+                    <div class="col-span-2 lg:col-span-2 grid items-center align-center relative lg:w-64">
+                        <x-input class="h-9 pr-10" type="search" wire:model="search" autofocus />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 mr-2 fill-current text-custom-violet absolute right-0" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,7 +65,7 @@
                                         Invoice Number
                                     </th>
                                     <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        Ordered By
+                                        Customer Name
                                     </th>
                                     <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                         Products
@@ -144,7 +185,7 @@
                                     <td class="md:px-6 py-4 text-center" colspan="6">
                                         <div>
                                             <span class=" text-2xl font-semibold text-gray-400 leading-tight">
-                                                {{ __('You have no orders!') }}
+                                                {{ __('There are no orders!') }}
                                             </span>
                                         </div>
                                     </td>
