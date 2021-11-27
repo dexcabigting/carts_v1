612c45fe9263bef3c8eeb45e25c85e000052104b
dexcabigting <dexcabigting1234@gmail.com>
2021-11-27 23:57:05 +0800

    Created ProfileDeleteAddress component

---
+++ app/Http/Livewire/Profile/ProfileDeleteAddress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Profile;
+
+use Livewire\Component;
+
+class ProfileDeleteAddress extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.profile-delete-address');
+    }
+}
