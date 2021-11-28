64379636a9ca4b1875397725061de3a80ef808bc
dexcabigting <dexcabigting1234@gmail.com>
2021-11-28 20:56:38 +0800

    Added user_address_id and discount on orders table

--- database/migrations/2021_10_19_074809_create_orders_table.php
+++ database/migrations/2021_10_19_074809_create_orders_table.php
@@ -16,12 +16,15 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('user_address_id');
             $table->string('invoice_number');
             $table->float('transaction_fee', 8, 2);
+            $table->float('discount', 8, 2);
             $table->string('status');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_address_id')->references('id')->on('user_addresses')->onDelete('cascade');       
         });
     }
 
