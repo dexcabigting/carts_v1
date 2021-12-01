93a16c12da18ea2adb310ba005b258125fa26b0d
dexcabigting <dexcabigting1234@gmail.com>
2021-12-01 23:00:47 +0800

    Added getOrderProperty for dynamic rendering

--- app/Http/Livewire/Orders/OrdersView.php
+++ app/Http/Livewire/Orders/OrdersView.php
@@ -7,12 +7,17 @@ use App\Models\Order;
 
 class OrdersView extends Component
 {
-    public $userOrder; 
+    // public $userOrder; 
+    public $orderId;
     public $orderStatuses;
     public $selectedStatus;
 
     public function mount($id)
     {
+        $this->orderId = $id;
+
+        $this->selectedStatus = $this->order->first()->status;
+
         $this->orderStatuses = [
             'Pending',
             'Approved',
@@ -21,23 +26,35 @@ class OrdersView extends Component
             'Delivered',
         ];
 
-        $this->userOrder = Order::where('id', $id)
+        // $this->userOrder = Order::where('id', $id)
+        //                     ->with(['user:id,name,email,phone', 
+        //                         'order_variants:id,order_id,amount,product_variant_id', 
+        //                         'order_variants.product_variant' => function ($query) {
+        //                             $query->select('id', 'product_id', 'prd_var_name')
+        //                             ->with(['product:id,prd_name']);
+        //                     }, 'order_variants.order_items'])
+        //                     ->withCount('order_items')
+        //                     ->first();
+        // dd($this->orderStatuses, $this->userOrder->status);
+    }
+
+    public function render()
+    {
+        $userOrder = $this->order->first();
+
+        return view('livewire.orders.orders-view', compact('userOrder'));
+    }
+
+    public function getOrderProperty()
+    {
+        return Order::where('id', $this->orderId)
                             ->with(['user:id,name,email,phone', 
                                 'order_variants:id,order_id,amount,product_variant_id', 
                                 'order_variants.product_variant' => function ($query) {
                                     $query->select('id', 'product_id', 'prd_var_name')
                                     ->with(['product:id,prd_name']);
                             }, 'order_variants.order_items'])
-                            ->withCount('order_items')
-                            ->first();
-
-        $this->selectedStatus = $this->userOrder->status;
-        // dd($this->orderStatuses, $this->userOrder->status);
-    }
-
-    public function render()
-    {
-        return view('livewire.orders.orders-view');
+                            ->withCount('order_items');
     }
 
     public function updateStatus()
@@ -47,7 +64,6 @@ class OrdersView extends Component
         ]);
 
         $this->emitUp('refreshParent');
-
     }
 
     public function closeViewModal()
