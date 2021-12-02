41d011ffee0f355a6cea63c170e1a04bf6a66895
dexcabigting <dexcabigting1234@gmail.com>
2021-12-02 15:09:13 +0800

    CheckoutIndex component now dispatches OrderCreated event successfully

--- app/Listeners/OrderCreatedNotifyAdmins.php
+++ app/Listeners/OrderCreatedNotifyAdmins.php
@@ -6,6 +6,10 @@ use App\Events\OrderCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\OrderCreatedNotification;
+
 class OrderCreatedNotifyAdmins
 {
     /**
@@ -27,5 +31,8 @@ class OrderCreatedNotifyAdmins
     public function handle(OrderCreated $event)
     {
         //
+        $admins = User::where('role_id', 1)->get();
+
+        Notification::send($admins, new OrderCreatedNotification($event->order));
     }
 }
