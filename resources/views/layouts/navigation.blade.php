ba71b80f04f9ab370a5109cf070078a4ef0e2749
dexcabigting <dexcabigting1234@gmail.com>
2021-12-04 14:55:17 +0800

    Added unstyled notification bell

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,6 +71,18 @@
         <div class="flex justify-between h-16">
             <!-- Settings Dropdown -->
             <div class="my-8 sm:flex sm:items-center sm:ml-6 text hidden lg:inline-flex">
+                <div class="flex flex-row text-white"> 
+                    <div>
+                        <a href="{{ route('notifications.index') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />    
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="rounded-full h-10 w-10 bg-custom-violet text-center align-middle">
+                        {{ count(auth()->user()->unreadNotifications) }}
+                    </div>                    
+                </div>
                 <img src="{{ asset('/img/Group 19.png') }}" alt="" class="rounded-full w-10 h-10 mx-4 border-4 border-custom-violet">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
