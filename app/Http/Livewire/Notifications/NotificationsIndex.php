95baa5c91cc730a91cbebcfe30d4e2c8883c65cf
dexcabigting <dexcabigting1234@gmail.com>
2021-12-04 14:29:07 +0800

    Restructured NotificationsIndex component

--- app/Http/Livewire/Notifications/NotificationsIndex.php
+++ app/Http/Livewire/Notifications/NotificationsIndex.php
@@ -11,10 +11,7 @@ class NotificationsIndex extends Component
     use WithPagination;
 
     public $notificationType;
-
-    protected $listeners = [
-        'refresh' => '$refresh',
-    ];
+    public $notificationId;
 
     public function mount()
     {
@@ -24,9 +21,13 @@ class NotificationsIndex extends Component
     public function render()
     {
         if(auth()->user()->role_id == 1) {
+
             $layout = 'layouts.app';
+
         } else {    
+
             $layout = 'layouts.app-user';
+            
         }
 
         $notificationType = $this->notificationType;
@@ -36,24 +37,22 @@ class NotificationsIndex extends Component
         return view('livewire.notifications.notifications-index', compact('notifications'))->layout($layout);
     }
 
-    public function getAuthNotificationsProperty()
+    public function getNotificationProperty()
     {
-        $notificationType = $this->notificationType;
-
-        return auth()->user()->$notificationType();
+        return DatabaseNotification::findOrFail($this->notificationId);
     }
 
     public function markAsRead($id)
     {
-        $this->auth_notifications->where('id', $id)->markAsRead(); 
+        $this->notificationId = $id;
 
-        // $this->emit('refresh');
+        $this->notification->markAsRead(); 
     }
 
     public function delete($id)
     {
-        auth()->user()->unreadNotifications()->where('id', $id)->delete(); 
+        $this->notificationId = $id;
 
-        // $this->emit('refresh');
+        $this->notification($id)->delete();  
     }
 }
