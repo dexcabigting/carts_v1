3a7e8d2ef6928cd8c3d17e09657c7be615e56793
dexcabigting <dexcabigting1234@gmail.com>
2021-12-09 22:48:14 +0800

    Added filters for category, fabric, product, and product variant

--- app/Http/Livewire/Sales/SalesIndex.php
+++ app/Http/Livewire/Sales/SalesIndex.php
@@ -12,8 +12,14 @@ class SalesIndex extends Component
 {
     use WithPagination;
 
+    public $categoryId = '%%';
+    public $fabricId = '%%';
+    public $productId = '%%';
+    public $productVariantId = '%%';
+
     public function render()
     {
+        dd($this->sales->get()->toArray());
         $sales = $this->sales->paginate(10);
         return view('livewire.sales.sales-index', compact('sales'))->layout('layouts.app');
     }
@@ -21,18 +27,21 @@ class SalesIndex extends Component
     public function getSalesProperty()
     {
         return OrderVariant::select('product_variant_id', 
-                                    DB::raw('sum(amount) as amount'), 
-                                    DB::raw('count(*) as quantity'),
-                                    DB::raw('min(created_at) as earliest'),
-                                    DB::raw('max(created_at) as latest'))
-                            ->groupBy('product_variant_id')
-                            ->with(['product_variant' => function ($query) {
-                                $query->select('id', 'product_id', 'prd_var_name')
-                                    ->with(['product' => function ($query) {
-                                        $query->select('id', 'prd_name');
-                                    }]);
-                            }]);
-                            // ->whereDate('created_at', '>=', '2021-12-06')
-                            // ->whereDate('created_at', '<=', '2021-12-07');
+                            DB::raw('sum(amount) as amount'), 
+                            DB::raw('count(*) as quantity'),
+                            DB::raw('min(created_at) as earliest'),
+                            DB::raw('max(created_at) as latest'))
+                    ->groupBy('product_variant_id')
+                    ->with(['product_variant' => function ($query) {
+                            $query->select('id', 'product_id', 'prd_var_name');
+                        }])
+                    ->whereHas('product_variant', function ($query) {
+                            $query->whereHas('product', function ($query) {
+                                    $query->where('category_id', 'like', $this->categoryId)
+                                        ->where('fabric_id', 'like', $this->fabricId);
+                                    })
+                                ->where('product_id', 'like', $this->productId);
+                        })
+                    ->where('product_variant_id', 'like', $this->productVariantId);                    
     }
 }
