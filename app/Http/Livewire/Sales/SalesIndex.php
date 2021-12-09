7416b85bf8cf0f0d10d593f19a3e1224d91d33e1
dexcabigting <dexcabigting1234@gmail.com>
2021-12-09 21:10:55 +0800

    Used WithPagination trait

--- app/Http/Livewire/Sales/SalesIndex.php
+++ app/Http/Livewire/Sales/SalesIndex.php
@@ -3,23 +3,26 @@
 namespace App\Http\Livewire\Sales;
 
 use Livewire\Component;
-use App\Models\Order;
+// use App\Models\Order;
 use App\Models\OrderVariant;
 use Illuminate\Support\Facades\DB;
+use Livewire\WithPagination;
 
 class SalesIndex extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        dd($this->sales);
-        return view('livewire.sales.sales-index')->layout('layouts.app');
+        $sales = $this->sales->paginate(10);
+        return view('livewire.sales.sales-index', compact('sales'))->layout('layouts.app');
     }
 
     public function getSalesProperty()
     {
         return OrderVariant::select('product_variant_id', 
                                     DB::raw('sum(amount) as amount'), 
-                                    DB::raw('count(*) as variant'),
+                                    DB::raw('count(*) as quantity'),
                                     DB::raw('min(created_at) as earliest'),
                                     DB::raw('max(created_at) as latest'))
                             ->groupBy('product_variant_id')
@@ -28,9 +31,8 @@ class SalesIndex extends Component
                                     ->with(['product' => function ($query) {
                                         $query->select('id', 'prd_name');
                                     }]);
-                            }])
-                            ->whereDate('created_at', '>=', '2021-12-06')
-                            ->whereDate('created_at', '<=', '2021-12-07')
-                            ->get()->toArray();
+                            }]);
+                            // ->whereDate('created_at', '>=', '2021-12-06')
+                            // ->whereDate('created_at', '<=', '2021-12-07');
     }
 }
