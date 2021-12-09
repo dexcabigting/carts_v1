55c0ba9b1ecfa69dddd43c768926542f2fd13274
dexcabigting <dexcabigting1234@gmail.com>
2021-12-09 21:11:46 +0800

    Sales are now rendered with no filters

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -1,5 +1,5 @@
 <div class="h-screen">
-    <div class="pt-12 pb-6">
+    {{--<div class="pt-12 pb-6">
         <div class="max-w-5xl mx-auto">
             <div class="bg-custom-blacki overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="flex flex-row gap-5 p-6 bg-custom-blacki shadow-2xl overflow-x-auto">
@@ -46,6 +46,130 @@
                         </svg>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>--}}
+
+    <div class="max-w-5xl mx-auto">
+        <div class="flex flex-col">
+            <div class="my-2 overflow-x-auto">
+                <div class="py-2 align-middle inline-block min-w-full">
+                    <div class="shadow overflow-hidden  border-gray-200 sm:rounded-lg">
+                        <table class="table-auto min-w-full divide-y divide-gray-200">
+                            <thead class="bg-custom-blacki">
+                                <tr>
+                                    <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        No.
+                                    </th>
+                                    {{--<th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Product
+                                    </th>--}}
+                                    <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Variant
+                                    </th>
+                                    <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Quantity
+                                    </th>
+                                    <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Amount
+                                    </th>
+                                    <th scope="col" class="md:px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Date Ordered
+                                    </th>
+                                </tr>
+                            </thead>
+
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @php
+                                    $totalAmount = 0;
+                                @endphp
+
+                                @forelse($sales as $index => $sale)
+                                <tr>
+                                    <td class="md:px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{ $sales->firstItem() + $index }}
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            {{ $sale->product_variant->prd_var_name }}
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            x{{ $sale->quantity }}
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            &#8369;{{ number_format($sale->amount, 2) }}
+
+                                            @php
+                                                $totalAmount = $totalAmount + $sale->amount;
+                                            @endphp
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 whitespace-nowrap">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            @if($sale->earliest->toFormattedDateString() == $sale->latest->toFormattedDateString())
+                                                {{ $sale->earliest->toFormattedDateString() }}
+                                            @else
+                                                {{ $sale->earliest->toFormattedDateString() }} to {{ $sale->latest->toFormattedDateString() }}
+                                            @endif
+                                        </div>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr>
+                                    <td class="md:px-6 py-4 text-center" colspan="4">
+                                        <div>
+                                            <span class=" text-2xl font-semibold text-gray-400 leading-tight">
+                                                {{ __('There are no sales!') }}
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforelse
+                                <tr>
+                                    <td class="md:px-6 py-4 text-center" colspan="2">
+                                    </td>
+
+                                    <td class="md:px-6 py-4 text-right">
+                                        <div>
+                                            <span class="font-semibold text-xl text-gray-800 leading-tight">
+                                                Total: 
+                                            </span>
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 text-left">
+                                        <div>
+                                            <span class="font-semibold text-xl text-gray-800 leading-tight">
+                                                &#8369;{{ number_format($totalAmount, 2) }}
+                                            </span>
+                                        </div>
+                                    </td>
+
+                                    <td class="md:px-6 py-4 text-center" colspan="1">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pb-12">
+        <div class="max-w-2xl mx-auto sm:md:px-6 lg:px-8">
+            <div class="mt-6">
+                {{ $sales->onEachSide(5)->links() }}
             </div>
         </div>
     </div>
