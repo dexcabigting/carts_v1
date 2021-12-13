e9fbdc43e14a45261950a35bdb51bff8b6a0b8f0
dexcabigting <dexcabigting1234@gmail.com>
2021-12-13 18:42:56 +0800

    Added JQuery and Ph Locations scripts on app.balde

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,12 @@
         
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- JQuery -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.js"></script>
+
+        <!-- Ph Locations -->
+        <script src="https://f001.backblazeb2.com/file/buonzz-assets/jquery.ph-locations-v1.0.0.js"></script>
         
         @livewireStyles
     </head>
