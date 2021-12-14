85c1072f823d957e36711cb77a4bb3bdab3de827
dexcabigting <dexcabigting1234@gmail.com>
2021-12-15 00:11:20 +0800

    Used SoftDeletes trait on user_addresses table

--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserAddress extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
@@ -18,6 +19,13 @@ class UserAddress extends Model
         'home_address',
         'is_main_address',
     ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
 
     public function user()
     {
