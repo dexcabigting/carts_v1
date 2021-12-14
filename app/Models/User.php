4cf00729a468060bcff6f2ae4f0a18b3ce39b028
dexcabigting <dexcabigting1234@gmail.com>
2021-12-15 00:10:55 +0800

    Fixed indention

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
         'created_at',
         'updated_at',
         'deleted_at'
-];
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
