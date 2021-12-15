ee47c7e0dc117bde4d8a8231bea1f5f0d1479b7a
dexcabigting <dexcabigting1234@gmail.com>
2021-12-15 15:51:20 +0800

    Now renders UsersEdit with address

--- app/Http/Livewire/Users/UsersEdit.php
+++ app/Http/Livewire/Users/UsersEdit.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\Users;
 use Livewire\Component;
 
 use App\Models\User;
+use App\Models\UserAddress;
+use App\Models\Role;
 
 use Yajra\Address\Entities\Region;
 use Yajra\Address\Entities\Province;
@@ -18,9 +20,27 @@ use App\Rules\PhoneNumber;
 class UsersEdit extends Component
 {
     public $user;
-
-    public $form = [];
+    public $form = [
+        'name' => '',
+        'email' => '',
+        'phone' => '',
+        'role_id' => '',
+        'verify_email' => '',
+        'region' => '',
+        'province' => '',
+        'city' => '',
+        'barangay' => '',
+        'home_address' => '',
+        'is_main_address' => ''
+    ];
     public $selectedAddress;
+    public $roles = [];
+    public $yesOrNo = [];
+    public $regions = [];
+    public $selectedRegion = '';
+    public $selectedProvince = '';
+    public $selectedCity = '';
+    public $selectedBarangay = '';
 
     protected function rules()
     {   
@@ -30,6 +50,14 @@ class UsersEdit extends Component
             'form.name' => 'required|string|max:255',
             'form.email' => 'required|string|email|max:255|unique:users,email,' . $this->user->id,
             'form.phone' => ['required', 'string', 'unique:users,phone,' . $this->user->id, new PhoneNumber($service)],
+            'form.role_id' => ['required', 'exists:roles,id'],
+            'form.verify_email' => ['required', 'in:Yes,No'],
+            'form.region' => ['required', 'string', 'exists:regions,name'],
+            'form.province' => ['required', 'string', 'exists:provinces,name'],
+            'form.city' => ['required', 'string', 'exists:cities,name'],
+            'form.barangay' => ['required', 'string', 'exists:barangays,name'],
+            'form.home_address' => ['required', 'string'],
+            'form.is_main_address' => ['required', 'string']
         ];
     }
 
@@ -37,22 +65,101 @@ class UsersEdit extends Component
         'form.name' => 'name',
         'form.email' => 'email address',
         'form.phone' => 'phone number',
+        'form.role_id' => 'role_id',
+        'form.verify_email' => 'verify email',
+        'form.region' => 'region',
+        'form.province' => 'province',
+        'form.city' => 'city',
+        'form.barangay' => 'barangay',
+        'form.home_address' => 'home address',
+        'form.is_main_address' => 'default'
     ];
 
     public function mount(User $id)
     {
-        $this->form = $id;
+        $this->form = [
+            'name' => $id->name,
+            'email' => $id->email,
+            'phone' => $id->phone,
+            'role_id' => $id->role_id,
+        ];
 
         $this->user = $id;
 
         $this->selectedAddress = $this->user->userAddresses()
                                     ->where('is_main_address', 1)
                                     ->first()->id; 
+
+        $this->roles = Role::get(['id', 'role'])->toArray();
+
+        $this->yesOrNo = ['Yes', 'No'];
+
+        $this->regions = Region::get(['name', 'region_id'])->toArray();
     }
 
     public function render()
     {
-        return view('livewire.users.users-edit');
+        $userAddress = $this->user_address->first();
+
+        $userAddresses = $this->user->userAddresses()
+                                ->get()->pluck('id')->toArray();
+
+        $this->selectedRegion = Region::where('name', $userAddress->region)->first()->region_id;
+        $this->selectedProvince = Province::where('name', $userAddress->province)->first()->province_id;
+        $this->selectedCity = City::where('name', $userAddress->city)->first()->city_id;
+        $this->selectedBarangay = Barangay::where('name', $userAddress->barangay)->first()->id;
+
+        $provinces = $this->provinces->toArray();
+        $cities = $this->cities->toArray();
+        $barangays = $this->barangays->toArray();
+
+        return view('livewire.users.users-edit', compact('userAddress', 'userAddresses', 'provinces', 'cities', 'barangays'));
+    }
+
+    public function getUserAddressProperty()
+    {
+        return UserAddress::where('id', $this->selectedAddress);
+    }
+
+    public function getProvincesProperty()
+    {
+        return Province::where('region_id', $this->selectedRegion)
+            ->get(['name', 'region_id', 'province_id']);
+    }
+
+    public function getCitiesProperty()
+    {
+        return City::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->get(['name', 'region_id', 'province_id', 'city_id']);
+    }
+
+    public function getBarangaysProperty()
+    {
+        return Barangay::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->where('city_id', $this->selectedCity)
+            ->get(['id', 'name']);
+    }
+
+    public function getRegionProperty()
+    {
+        return Region::where('region_id', $this->selectedRegion)->first('name');
+    }
+
+    public function getProvinceProperty()
+    {
+        return Province::where('province_id', $this->selectedProvince)->first('name');
+    }
+
+    public function getCityProperty()
+    {
+        return City::where('city_id', $this->selectedCity)->first('name');
+    }
+
+    public function getBarangayProperty()
+    {
+        return Barangay::where('id', $this->selectedBarangay)->first('name');
     }
 
     public function update()
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,41 +1,157 @@
-<div class="p-5">
+<div class="max-w-7xl p-5">
     <x-success-fail-message />
     <x-validation-errors :errors="$errors" />
 
-    <h2 class="font-semibold text-l text-gray-100 leading-tight mb-4">
-        {{ __('User Credentials') }}
-    </h2>
-
-    <form wire:submit.prevent="update">
-        @csrf
-
+    <div class="flex flex-col bg-custom-blacki justify-center items-center">
         <div>
-            <x-label :value="__('Name')"/>
-            <x-input wire:model="form.name" class="block mt-1 w-full text-black" type="text" autofocus required />
+            <h2 class="font-semibold text-xl text-gray-100 leading-tight">
+                {{ __('User Credentials') }} 
+            </h2>
         </div>
 
-        <div class="mt-4">
-            <x-label :value="__('Email')" />
-            <x-input wire:model="form.email" class="block mt-1 w-full text-black" type="email" required />
-        </div>
+        <form class="bg-custom-blacki" wire:submit.prevent="update(Object.fromEntries(new FormData($event.target)))">
+            @csrf
+            <div class="flex flex-row gap-5">
+                <div class="flex flex-col gap-5">
+                    <div class="text-gray-900">
+                        <x-label  for="name" :value="__('Name')"/>
+                        <x-input wire:model.defer="form.name" id="name" class="block mt-1 w-full text-black" type="text" name="name" autofocus required />
+                    </div>
 
-        <div class="mt-4">
-            <x-label :value="__('Phone')" />
-            <x-input wire:model="form.phone" class="block mt-1 w-full text-black" type="text" required />
-        </div>
+                    <div class="">
+                        <x-label for="email" :value="__('Email')" />
+                        <x-input wire:model.defer="form.email" id="email" class="block mt-1 w-full text-black" type="email" name="email" required />
+                    </div>
 
-        <div class="flex mt-4 gap-5 justify-center">
-            <div>
-                <x-button class="hover:bg-purple-900 hover:text-purple-100 text-xl font-semibold text-white w-full px-4 py-2 bg-custom-violet">
-                    {{ __('Update Credentials') }}
-                </x-button>
+                    <div class="">
+                        <x-label for="phone" :value="__('Phone')" />
+                        <x-input wire:model.defer="form.phone" id="phone" class="block mt-1 w-full text-black" type="text" name="phone" required />
+                    </div>
+
+                    <div class="flex flex-row gap-5">
+                        <div class="w-1/2">
+                            <x-label :value="__('Role')" />
+                            @foreach($roles as $role)
+                                <div wire:key="{{ $loop->index }}-role">
+                                    <input wire:model="form.role_id" type="radio" value="{{ $role['id'] }}" required/>
+                                    <span>{{ $role['role'] }}</span>
+                                </div>
+                            @endforeach
+                        </div>
+
+                        <div class="w-1/2">
+                            <x-label :value="__('Verify Email?')" />
+                            @foreach($yesOrNo as $option)
+                                <div wire:key="{{ $loop->index }}-option">
+                                    <input name="verify_email" type="radio" value="{{ $option }}" required/>
+                                    <span>{{ $option }}</span>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>  
+                </div>
+
+                <div>
+                    <div class="text-sm font-medium text-gray-900">
+                        <select wire:model="selectedAddress">
+                            @foreach($userAddresses as $index => $address)
+                                <option value="{{ $address }}">
+                                    Address {{ $index + 1 }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    
+                    <div>
+                        <x-label class="text-xl mb-4 text-white" :value="__('Home Address')" />
+
+                        <x-input wire:model.defer="form.home_address" class="block mt-1 w-full text-black" type="text" placeholder="ex. Street Apartment no." />
+                    </div>
+
+                    <!-- Address -->
+                    <div class="block md:flex fle-row">
+                        <div class="mt-4 ">
+                            <x-label :value="__('Region')" />
+                            <select wire:model="selectedRegion" class="text-black rounded-lg lg:pr-16 pr-4 w-full">
+                                <option value="" selected="selected">
+                                    Region
+                                </option>
+                                @foreach($regions as $region)
+                                    <option value="{{ $region['region_id'] }}">
+                                        {{ $region['name'] }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="mt-4 md:ml-4">
+                            <x-label :value="__('Province')" />
+                            <select wire:model="selectedProvince" class="text-black rounded-lg md:pr-20 w-full md:w-auto" id="create_province">
+                                <option value="" selected="selected">
+                                    Province
+                                </option>
+                                @foreach($provinces as $province)
+                                    <option value="{{ $province['province_id'] }}">
+                                        {{ $province['name'] }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    
+                    <div class="mt-4">
+                        <x-label :value="__('City')" />
+                        <select wire:model="selectedCity" class="text-black rounded-lg w-full">
+                            <option value="" selected="selected">
+                                City
+                            </option>
+                             @foreach($cities as $city)
+                                <option value="{{ $city['city_id'] }}">
+                                    {{ $city['name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="mt-4">
+                        <x-label for="create_barangay" :value="__('Barangay')"  />
+                        <select wire:model="selectedBarangay" class="text-black rounded-lg w-full">
+                            <option value="" selected="selected">
+                                Barangay
+                            </option>
+                             @foreach($barangays as $barangay)
+                                <option value="{{ $barangay['id'] }}">
+                                    {{ $barangay['name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="mt-4">
+                        <x-label :value="__('Set as Default')"  />
+                            <input type="radio" name="is_main_address" value="1" checked/>
+                            <label>Yes</label><br>
+                            <input type="radio" name="is_main_address" value="0" />
+                            <label >No</label><br>
+                    </div>
+                </div>
             </div>
-            
-            <div>
-                <x-button wire:click.prevent="closeEditModal()" type="button" class="bg-red-500 text-gray-100 text-xl font-bold px-4 py-2">
-                    {{ __('Close') }}
-                </x-button>
-            </div>  
-        </div>
-    </form>
+
+             <div class="flex gap-5 justify-center">
+                <div>
+                    <x-button class="hover:bg-purple-900 hover:text-purple-100 text-xl font-semibold text-white w-full px-4 py-2 bg-custom-violet">
+                        {{ __('Update User') }}
+                    </x-button>
+                </div>
+                
+                <div>
+                    <x-button wire:click.prevent="closeEditModal()" type="button" class="bg-red-500 text-gray-100 text-xl font-bold px-4 py-2">
+                        {{ __('Close') }}
+                    </x-button>
+                </div>  
+            </div>
+        </form>
+    </div>
 </div>
+
+    
