c32ac5fe3bb1c8c5878f1e5e84f2c59320c84c78
dexcabigting <dexcabigting1234@gmail.com>
2021-12-15 12:02:15 +0800

    Swapped Order By and Users filters

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -25,6 +25,28 @@
                   
                     </div>
 
+                    <!-- Users -->
+                    <div class="xl:ml-0">
+                        <div class="font-medium text-gray-100 py-4">
+                            <div class="flex flex-col items-center justify-items-center lg:flex-row mx-2">
+                                <div class="mx-2">
+                                <x-label :value="__('Users')" class="text-gray-50 inline-block font-bold text-sm mx-1 xl:text-xl" />
+                                </div>
+                                <div class="inline-flex">
+                                    <select wire:model="query" class="text-sm font-medium bg-custom-black text-white rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'">
+                                        <option value="users">
+                                                <x-label :value="__('Users')" class="inline-block" />
+                                        </option>
+
+                                        <option value="deletedUsers">
+                                            <x-label :value="__('Deleted Users')" class="inline-block" />
+                                        </option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <!-- Order By -->
                     <div class="xl:ml-0">
                         <div class="font-medium text-gray-100 py-4">
@@ -49,28 +71,6 @@
 
                                         <option value="desc">
                                             <x-label :value="__('Descending')" class="inline-block" />
-                                        </option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Users -->
-                    <div class="xl:ml-0">
-                        <div class="font-medium text-gray-100 py-4">
-                            <div class="flex flex-col items-center justify-items-center lg:flex-row mx-2">
-                                <div class="mx-2">
-                                <x-label :value="__('Users')" class="text-gray-50 inline-block font-bold text-sm mx-1 xl:text-xl" />
-                                </div>
-                                <div class="inline-flex">
-                                    <select wire:model="query" class="text-sm font-medium bg-custom-black text-white rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'">
-                                        <option value="users">
-                                                <x-label :value="__('Users')" class="inline-block" />
-                                        </option>
-
-                                        <option value="deletedUsers">
-                                            <x-label :value="__('Deleted Users')" class="inline-block" />
                                         </option>
                                     </select>
                                 </div>
@@ -194,28 +194,42 @@
                                 </td>
 
                                 <td class="flex md:px-6 px-2 py-4 whitespace-nowrap">
-                                    <div>
-                                        <button wire:click.prevent="openEditModal({{ $user->id }})" class="p-2 bg-green-600 border border-transparent font-semibold text-xs text-white uppercase tracking-wide hover:bg-green-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                                            <span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
-                                                    <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
-                                                </svg>
-                                                {{ __('Edit') }}
-                                            </span>
-                                        </button>
-                                    </div>
+                                    @if(!$user->trashed())
+                                        <div>
+                                            <button wire:click.prevent="openEditModal({{ $user->id }})" class="p-2 bg-green-600 border border-transparent font-semibold text-xs text-white uppercase tracking-wide hover:bg-green-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                                <span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                                    </svg>
+                                                    {{ __('Edit') }}
+                                                </span>
+                                            </button>
+                                        </div>
 
-                                    <div>
-                                        <button wire:click.prevent="openDeleteModal({{ $user->id }})" class="p-2 border border-transparent font-semibold text-xs bg-red-600 text-white uppercase tracking-normal hover:bg-red-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                                            <span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
-                                                    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                                                </svg>
-                                                {{ __('Delete') }}
-                                            </span>
-                                        </button>
-                                    </div>
+                                        <div>
+                                            <button wire:click.prevent="openDeleteModal({{ $user->id }})" class="p-2 border border-transparent font-semibold text-xs bg-red-600 text-white uppercase tracking-normal hover:bg-red-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                                <span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                                    </svg>
+                                                    {{ __('Delete') }}
+                                                </span>
+                                            </button>
+                                        </div>
+                                    @else
+                                        <div>
+                                            <button wire:click.prevent="restoreUser({{ $user->id }})" class="p-2 bg-green-600 border border-transparent font-semibold text-xs text-white uppercase tracking-wide hover:bg-green-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                                                <span>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                                    </svg>
+                                                    {{ __('Restored') }}
+                                                </span>
+                                            </button>
+                                        </div>
+                                    @endif
                                 </td>
 
                                 <td class="md:px-6 px-2 py-4 whitespace-nowrap">
