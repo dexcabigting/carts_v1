9207de5facec267de5a497cc63875c9fcbda3c20
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 00:16:55 +0800

    RegisterIndex component is now fully functional

--- app/Http/Livewire/Auth/RegisterIndex.php
+++ app/Http/Livewire/Auth/RegisterIndex.php
@@ -4,8 +4,81 @@ namespace App\Http\Livewire\Auth;
 
 use Livewire\Component;
 
+use App\Models\User;
+
+use Illuminate\Auth\Events\Registered;
+
+use Yajra\Address\Entities\Region;
+use Yajra\Address\Entities\Province;
+use Yajra\Address\Entities\City;
+use Yajra\Address\Entities\Barangay;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+use App\Service\Twilio\PhoneNumberLookupService;
+
+use App\Rules\PhoneNumber;
+
 class RegisterIndex extends Component
 {
+    public $form = [
+        'name' => '',
+        'email' => '',
+        'phone' => '',
+        'password' => '',
+        'password_confirmation' => '',
+        'region' => '',
+        'province' => '',
+        'city' => '',
+        'barangay' => '',
+        'home_address' => '',
+    ];
+    public $regions = [];
+    public $provinces = [];
+    public $cities = [];
+    public $barangays = [];
+    public $selectedRegion = "";
+    public $selectedProvince = "";
+    public $selectedCity = "";
+    public $selectedBarangay = "";
+
+    protected function rules()
+    {
+        $service = app()->make(PhoneNumberLookupService::class);
+        
+        return [
+            'form.name' => ['required', 'string', 'max:255'],
+            'form.email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'form.phone' => ['required', 'string', 'unique:users,phone', new PhoneNumber($service)],
+            'form.password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'form.region' => ['required', 'string', 'exists:regions,name'],
+            'form.province' => ['required', 'string', 'exists:provinces,name'],
+            'form.city' => ['required', 'string', 'exists:cities,name'],
+            'form.barangay' => ['required', 'string', 'exists:barangays,name'],
+            'form.home_address' => ['required', 'string']
+        ];
+    }
+
+    protected $validationAttributes = [
+        'form.name' => 'name',
+        'form.email' => 'email address',
+        'form.phone' => 'phone number',
+        'form.password' => 'password',
+        'form.password_confirmation' => 'confirm password',
+        'form.region' => 'region',
+        'form.province' => 'province',
+        'form.city' => 'city',
+        'form.barangay' => 'barangay',
+        'form.home_address' => 'home address',
+    ];
+
+    public function mount()
+    {
+        $this->regions = Region::get(['name', 'region_id'])->toArray();
+    }
+
     public function render()
     {
         return view('livewire.auth.register-index')->layout('layouts.guest');
@@ -13,6 +86,115 @@ class RegisterIndex extends Component
 
     public function store()
     {
+        // dd($this->selectedBarangay);
+        $phone = preg_replace('/^(09)(\d+)/', '639$2', $this->form['phone']);
+
+        $this->form['phone'] = $phone;
+
+        $this->form['region'] = (empty($this->selectedRegion)) ? "" : $this->region->name;
+        $this->form['province'] = (empty($this->selectedProvince)) ? "" : $this->province->name;
+        $this->form['city'] = (empty($this->selectedCity)) ? "" : $this->city->name;
+
+        if(!empty($this->barangays)) {
+            $this->form['barangay'] = (empty($this->selectedBarangay)) ? "" : $this->barangay->name;
+        } elseif(empty($this->barangays)) {
+            $this->form['barangay'] = (!empty($this->selectedCity) && empty($this->selectedBarangay)) ? 'N/A' : "" ;
+        }
+
+        // dd( $this->form['barangay']);
+
+        $this->validate();
         
+        $user = User::create([
+            'name' => $this->form['name'],
+            'email' => $this->form['email'],
+            'phone' => $this->form['phone'],
+            'password' => Hash::make($this->form['password']),
+        ]);
+
+        $user->userAddresses()->create([
+            'region' => $this->form['region'],
+            'province' => $this->form['province'],
+            'city' => $this->form['city'],
+            'barangay' => $this->form['barangay'],
+            'home_address' => $this->form['home_address'],
+            'is_main_address' => 1,
+        ]);
+
+        $this->reset(['form', 'selectedRegion', 'selectedProvince', 'selectedCity', 'selectedBarangay', 'provinces', 'cities', 'barangays']);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect()->route('shop.index'); 
+    }
+
+    public function getProvinceListProperty()
+    {
+        return Province::where('region_id', $this->selectedRegion)
+            ->get(['name', 'region_id', 'province_id']);
+    }
+
+    public function getCityListProperty()
+    {
+        return City::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->get(['name', 'region_id', 'province_id', 'city_id']);
+    }
+
+    public function getBarangayListProperty()
+    {
+        return Barangay::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->where('city_id', $this->selectedCity)
+            ->get(['id', 'name']);
+    }
+
+    public function getRegionProperty()
+    {
+        return Region::where('region_id', $this->selectedRegion)->first('name');
+    }
+
+    public function getProvinceProperty()
+    {
+        return Province::where('province_id', $this->selectedProvince)->first('name');
+    }
+
+    public function getCityProperty()
+    {
+        return City::where('city_id', $this->selectedCity)->first('name');
+    }
+
+    public function getBarangayProperty()
+    {
+        return Barangay::where('id', $this->selectedBarangay)->first('name');
+    }
+
+    public function updatedSelectedRegion()
+    {
+        $this->provinces = $this->province_list->toArray();
+        $this->selectedProvince = ""; 
+        $this->selectedCity = "";
+        $this->selectedBarangay = "";
+    }
+
+    public function updatedSelectedProvince($province)
+    {
+        if(!empty($province)) {
+            $this->cities = $this->city_list->toArray();
+        }
+
+        $this->selectedCity = "";
+        $this->selectedBarangay = "";
+    }
+
+    public function updatedSelectedCity($city)
+    {
+        if(!empty($city)) {
+            $this->barangays = $this->barangay_list->toArray();
+        }
+
+        $this->selectedBarangay = "";
     }
 }
--- resources/views/auth/register-index.blade.php
+++ resources/views/auth/register-index.blade.php
@@ -1,98 +1,132 @@
-<x-auth-card>
-    <div class="px-12">
-        <section class="w-full flex justify-center items-center my-24 flex-col">
-            <h1 class="font-semibold  text-7xl md:text-9xl text-custom-blacki my-4">REGISTER</h1>
-            <!-- Validation Errors -->
-            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<div>
+    <x-auth-card>
+        <div class="px-12">
+            <section class="w-full flex justify-center items-center my-24 flex-col">
+                <h1 class="font-semibold  text-7xl md:text-9xl text-custom-blacki my-4">REGISTER</h1>
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-            <form class="w-full max-w-lg" wire:submit.prevent="store">
-                @csrf
+                <form class="w-full max-w-lg" wire:submit.prevent="store">
+                    @csrf
 
-                <!-- Name -->
-                <div>
-                    <x-label class="text-xl text-purple-100  " for="name" :value="__('Name')" />
-                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-                </div>
-
-                <!-- Email Address -->
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100  " for="email" :value="__('Email')" />
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-                </div>
-
-                <!-- Phone -->
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100  " for="phone" :value="__('Phone')" />
-                    <x-input id="phone" class="block mt-1 w-full" type="tel" name="phone" :value="old('phone')" required />
-                </div>
-
-                <!-- Address -->
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100  " for="home_address" :value="__('Home Address')" />
-                    <x-input id="home_address" class="block mt-1 w-full" type="text" name="home_address" :value="old('home_address')" required />
-                </div>
-                <div class="mt-4">
-                    <x-label  class="text-xl text-purple-100" for="region" :value="__('Region')" />
-                    <select class="w-full rounded-lg" id="region">
-                        <option>Region</option>
-                    </select>
-                    <x-input id="region-text" type="hidden" name="region" :value="old('region')" />
-                </div>
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100" for="province" :value="__('Province')" />
-                    <select class="w-full rounded-lg" id="province">
-                        <option>
-                            Province
-                        </option>
-                    </select>
-                    <x-input id="province-text" type="hidden" name="province" :value="old('province')" />
-                </div>
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100" for="city" :value="__('City')" />
-
-                    <select class="w-full rounded-lg" id="city">
-                        <option>City</option>
-                    </select>
-                    <x-input id="city-text" type="hidden" name="city" :value="old('city')" />
-                </div>
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100" for="barangay" :value="__('Barangay')" />
-                    <select class="w-full rounded-lg" name="barangay" id="barangay">
-                        <option>Barangay</option>
-                    </select>
-                    <x-input id="barangay-text" type="hidden" name="barangay" :value="old('barangay')" />
-                </div>
-                
-
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100  " for="password" :value="__('Password')" />
-
-                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-                </div>
-
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-label class="text-xl text-purple-100  " for="password_confirmation" :value="__('Confirm Password')" />
-
-                    <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required />
-                </div>
-
-                <div class="flex items-center flex-col justify-center mt-4">
+                    <!-- Name -->
                     <div>
-                        <x-button class="md:mt-4 mt-12 uppercase hover:bg-purple-900 hover:text-purple-100 text-3xl font-semibold text-white px-32 md:px-44 py-6 my-6 w-full bg-custom-violet">
-                            {{ __('Register') }}
-                        </x-button>
+                        <x-label class="text-xl text-purple-100" :value="__('Name')" />
+                        <x-input wire:model.defer="form.name" class="block mt-1 w-full" type="text" :value="old('name')" required autofocus />
                     </div>
-                    <div>
-                        <a class="underline text-xl text-gray-600 hover:text-purple-200" href="{{ route('login') }}">
-                            {{ __('Already registered?') }}
-                        </a>
+
+                    <!-- Email Address -->
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Email')" />
+                        <x-input wire:model.defer="form.email" class="block mt-1 w-full" type="email" :value="old('email')" required />
                     </div>
-                </div>
-            </form>
-        </section>
-    </div>
-</x-auth-card>
+
+                    <!-- Phone -->
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Phone')" />
+                        <x-input wire:model.defer="form.phone" class="block mt-1 w-full" type="tel" :value="old('phone')" required />
+                    </div>
+
+                    <!-- Address -->
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Home Address')" />
+                        <x-input wire:model.defer="form.home_address" class="block mt-1 w-full" type="text" :value="old('home_address')" placeholder="ex. Street or Apartment no." required />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-label  class="text-xl text-purple-100" :value="__('Region')" />
+                        <select wire:model="selectedRegion" class="w-full rounded-lg">
+                            <option value="" selected>
+                                Region
+                            </option>
+                            @foreach($regions as $region)
+                                <option value="{{ $region['region_id'] }}">
+                                    {{ $region['name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    @if(!empty($selectedRegion))
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Province')" />
+                        <select wire:model="selectedProvince" class="w-full rounded-lg">
+                            <option value="" selected>
+                                Province
+                            </option>
+                            @foreach($provinces as $province)
+                                <option value="{{ $province['province_id'] }}">
+                                    {{ $province['name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    @endif
+
+                    @if(!empty($selectedProvince))
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('City')" />
+                        <select wire:model="selectedCity" class="w-full rounded-lg" >
+                            <option value="" selected>
+                                City
+                            </option>
+                            @foreach($cities as $city)
+                            <option value="{{ $city['city_id'] }}">
+                                {{ $city['name'] }}
+                            </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    @endif
+
+                    @if(!empty($selectedCity) && !empty($barangays))
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Barangay')" />
+                        <select wire:model="selectedBarangay" class="w-full rounded-lg">
+                            <option value="" selected>
+                                Barangay
+                            </option>
+                            @foreach($barangays as $barangay)
+                                <option value="{{ $barangay['id'] }}">
+                                    {{ $barangay['name'] }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    @elseif(!empty($selectedCity) && empty($barangays))
+                        <div class="mt-4">
+                            <x-label value="This city has no barangays." />
+                        </div>
+                    @endif
+                    
+                    <!-- Password -->
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Password')" />
+                        <x-input wire:model.defer="form.password" class="block mt-1 w-full" type="password" required autocomplete="new-password" />
+                    </div>
+
+                    <!-- Confirm Password -->
+                    <div class="mt-4">
+                        <x-label class="text-xl text-purple-100" :value="__('Confirm Password')" />
+                        <x-input wire:model.defer="form.password_confirmation" class="block mt-1 w-full" type="password" required />
+                    </div>
+
+                    <div class="flex items-center flex-col justify-center mt-4">
+                        <div>
+                            <x-button class="md:mt-4 mt-12 uppercase hover:bg-purple-900 hover:text-purple-100 text-3xl font-semibold text-white px-32 md:px-44 py-6 my-6 w-full bg-custom-violet">
+                                {{ __('Register') }}
+                            </x-button>
+                        </div>
+                        <div>
+                            <a class="underline text-xl text-gray-600 hover:text-purple-200" href="{{ route('login') }}">
+                                {{ __('Already registered?') }}
+                            </a>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </x-auth-card>
+</div>
 
     
