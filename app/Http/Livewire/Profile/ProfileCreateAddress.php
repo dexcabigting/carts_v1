5c7e919776b519173ea0e87068ad2df823b67f20
dexcabigting <dexcabigting1234@gmail.com>
2021-12-19 01:28:43 +0800

    ProfileCreateAddress successfully refactored

--- app/Http/Livewire/Profile/ProfileCreateAddress.php
+++ app/Http/Livewire/Profile/ProfileCreateAddress.php
@@ -3,19 +3,37 @@
 namespace App\Http\Livewire\Profile;
 
 use Livewire\Component;
-use App\Models\UserAddress;
+
+use Yajra\Address\Entities\Region;
+use Yajra\Address\Entities\Province;
+use Yajra\Address\Entities\City;
+use Yajra\Address\Entities\Barangay;
 
 class ProfileCreateAddress extends Component
 {
-    public $form;
+    public $form = [
+        'region' => '',
+        'province' => '',
+        'city' => '',
+        'barangay' => '',
+        'home_address' => '',
+        'is_main_address' => 1
+    ];
+    public $provinces = [];
+    public $cities = [];
+    public $barangays = [];
+    public $selectedRegion = "";
+    public $selectedProvince = "";
+    public $selectedCity = "";
+    public $selectedBarangay = "";
 
     protected $rules = [
-        'form.region' => 'required|string',
-        'form.province' => 'required|string',
-        'form.city' => 'required|string',
-        'form.barangay' => 'required|string',
+        'form.region' => 'required|string|exists:regions,name',
+        'form.province' => 'required|string|exists:provinces,name',
+        'form.city' => 'required|string|exists:cities,name',
+        'form.barangay' => 'required|string|exists:barangays,name',
         'form.home_address' => 'required|string',
-        'form.is_main_address' => 'required|string',
+        'form.is_main_address' => 'required|boolean',
     ];
 
     protected $validationAttributes = [
@@ -29,7 +47,7 @@ class ProfileCreateAddress extends Component
 
     public function mount()
     {
-        $this->form = [];
+        $this->regions = Region::get(['name', 'region_id'])->toArray();
     }
 
     public function render()
@@ -37,20 +55,21 @@ class ProfileCreateAddress extends Component
         return view('livewire.profile.profile-create-address');
     }
 
-    public function storeAddress($formData)
+    public function storeAddress()
     {
-        $this->form = [
-            'region' => $formData['create_region'],
-            'province' => $formData['create_province'],
-            'city' => $formData['create_city'],
-            'barangay' => $formData['create_barangay'],
-            'home_address' => $formData['create_home_address'],
-            'is_main_address' => $formData['is_main_address'],
-        ];
+        $this->form['region'] = (empty($this->selectedRegion)) ? "" : $this->region->name;
+        $this->form['province'] = (empty($this->selectedProvince)) ? "" : $this->province->name;
+        $this->form['city'] = (empty($this->selectedCity)) ? "" : $this->city->name;
+
+        if(!empty($this->barangays)) {
+            $this->form['barangay'] = (empty($this->selectedBarangay)) ? "" : $this->barangay->name;
+        } elseif(empty($this->barangays)) {
+            $this->form['barangay'] = (!empty($this->selectedCity) && empty($this->selectedBarangay)) ? 'N/A' : "" ;
+        }
 
         $this->validate();
 
-        if($this->form['is_main_address'] == "1") {
+        if($this->form['is_main_address'] == 1) {
             auth()->user()->userAddresses()
                 ->where('is_main_address', 1)
                 ->update(['is_main_address' => 0]);
@@ -65,16 +84,52 @@ class ProfileCreateAddress extends Component
             'is_main_address' => $this->form['is_main_address'],
         ]);
 
-        $this->clearFormFields();
+        $this->reset(['form', 'selectedRegion', 'selectedProvince', 'selectedCity', 'selectedBarangay', 'provinces', 'cities', 'barangays']);
 
         $this->emitUp('latestAddress');
 
         session()->flash('success', 'Address has been created successfully!'); 
     }
 
-    public function clearFormFields()
+    public function getProvinceListProperty()
     {
-        $this->form = [];
+        return Province::where('region_id', $this->selectedRegion)
+            ->get(['name', 'region_id', 'province_id']);
+    }
+
+    public function getCityListProperty()
+    {
+        return City::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->get(['name', 'region_id', 'province_id', 'city_id']);
+    }
+
+    public function getBarangayListProperty()
+    {
+        return Barangay::where('region_id', $this->selectedRegion)
+            ->where('province_id', $this->selectedProvince)
+            ->where('city_id', $this->selectedCity)
+            ->get(['id', 'name']);
+    }
+
+    public function getRegionProperty()
+    {
+        return Region::where('region_id', $this->selectedRegion)->first('name');
+    }
+
+    public function getProvinceProperty()
+    {
+        return Province::where('province_id', $this->selectedProvince)->first('name');
+    }
+
+    public function getCityProperty()
+    {
+        return City::where('city_id', $this->selectedCity)->first('name');
+    }
+
+    public function getBarangayProperty()
+    {
+        return Barangay::where('id', $this->selectedBarangay)->first('name');
     }
 
     public function closeCreateModal()
@@ -82,5 +137,32 @@ class ProfileCreateAddress extends Component
         $this->dispatchBrowserEvent('createModalDisplayNone');
 
         $this->emitUp('closeCreateModal');
+    }
+
+    public function updatedSelectedRegion()
+    {
+        $this->provinces = $this->province_list->toArray();
+        $this->selectedProvince = ""; 
+        $this->selectedCity = "";
+        $this->selectedBarangay = "";
+    }
+
+    public function updatedSelectedProvince($province)
+    {
+        if(!empty($province)) {
+            $this->cities = $this->city_list->toArray();
+        }
+
+        $this->selectedCity = "";
+        $this->selectedBarangay = "";
+    }
+
+    public function updatedSelectedCity($city)
+    {
+        if(!empty($city)) {
+            $this->barangays = $this->barangay_list->toArray();
+        }
+
+        $this->selectedBarangay = "";
     }
 }
--- resources/views/profile/create-address.blade.php
+++ resources/views/profile/create-address.blade.php
@@ -2,73 +2,103 @@
     <x-success-fail-message class="mb-4" />
     <x-validation-errors class="mb-4" :errors="$errors" />
 
-    <form wire:submit.prevent="storeAddress(Object.fromEntries(new FormData($event.target)))">
+    <form wire:submit.prevent="storeAddress">
         @csrf
+
         <div>
             <x-label class="text-xl mb-4 text-white" :value="__('Home Address')" />
-
-            <x-input id="home_address" class="block mt-1 w-full text-black" type="text" name="create_home_address" placeholder="ex. Street Apartment no." />
+            <x-input wire:model="form.home_address" class="block mt-1 w-full text-black" type="text" placeholder="ex. Street/Apartment no." />
         </div>
 
         <!-- Address -->
-        <div class="block md:flex fle-row">
+        <div class="block md:flex flex-row">
             <div class="mt-4 ">
-                <x-label for="create_region" :value="__('Region')" />
-                <select class="text-black rounded-lg lg:pr-16 pr-4 w-full" id="create_region">
-                    <option selected>
+                <x-label :value="__('Region')" />
+                <select wire:model="selectedRegion" class="text-black rounded-lg lg:pr-16 pr-4 w-full">
+                    <option value="" selected>
                         Region
                     </option>
+                    @foreach($regions as $region)
+                        <option wire:key="{{ $loop->index }}-region" value="{{ $region['region_id'] }}">
+                            {{ $region['name'] }}
+                        </option>
+                    @endforeach
                 </select>
-                <x-input id="create_region-text" name="create_region" type="hidden" />
             </div>
 
-            <div class="mt-4 md:ml-4">
-                <x-label for="create_province" :value="__('Home Address')" />
-                <select class="text-black rounded-lg md:pr-20 w-full md:w-auto" id="create_province">
-                    <option>
-                        <h1 class="px-12">Province</h1>
-                    </option>
-                </select>
-                <x-input id="create_province-text" name="create_province" type="hidden" />
-            </div>
+            @if(!empty($selectedRegion))
+                <div class="mt-4 md:ml-4">
+                    <x-label :value="__('Province')" />
+                    <select wire:model="selectedProvince" class="text-black rounded-lg md:pr-20 w-full md:w-auto">
+                       <option value="" selected>
+                            Province
+                        </option>
+                        @foreach($provinces as $province)
+                            <option wire:key="{{ $loop->index }}-province" value="{{ $province['province_id'] }}">
+                                {{ $province['name'] }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            @endif
         </div>
         
-        <div class="mt-4">
-            <x-label for="create_city" :value="__('City')" />
-            <select class="text-black rounded-lg w-full" id="create_city">
-                <option>
-                    City
-                </option>
-            </select>
-            <x-input id="create_city-text" name="create_city" type="hidden" />
-        </div>
+        @if(!empty($selectedProvince))
+            <div class="mt-4">
+                <x-label :value="__('City')" />
+                <select wire:model="selectedCity" class="text-black rounded-lg w-full">
+                    <option value="" selected>
+                        City
+                    </option>
+                    @foreach($cities as $city)
+                    <option wire:key="{{ $loop->index }}-city" value="{{ $city['city_id'] }}">
+                        {{ $city['name'] }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+        @endif
 
-        <div class="mt-4">
-            <x-label for="create_barangay" :value="__('Barangay')"  />
-            <select class="text-black rounded-lg w-full" id="create_barangay">
-                <option>
-                    Barangay
-                </option>
-            </select>
-            <x-input id="create_barangay-text" name="create_barangay" type="hidden" />
-        </div>
+        @if(!empty($selectedCity) && !empty($barangays))
+            <div class="mt-4">
+                <x-label :value="__('Barangay')"  />
+                <select wire:model="selectedBarangay" class="text-black rounded-lg w-full">
+                    <option value="" selected>
+                        Barangay
+                    </option>
+                    @foreach($barangays as $barangay)
+                        <option wire:key="{{ $loop->index }}-barangay" value="{{ $barangay['id'] }}">
+                            {{ $barangay['name'] }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        @elseif(!empty($selectedCity) && empty($barangays))
+            <div class="mt-4">
+                <x-label value="This city has no barangays." />
+            </div>
+        @endif
 
-        <div class="mt-4">
-            <x-label :value="__('Set as Default')"  />
-                <input type="radio" name="is_main_address" value="1" checked/>
-                <label>Yes</label><br>
-                <input type="radio" name="is_main_address" value="0" />
-                <label >No</label><br>
+        <div wire:key="2123-asdaqw" class="mt-4">
+            <x-label :value="__('Set as Default')" />
+            <div>
+                <input wire:model="form.is_main_address" type="radio" value="1"/>
+                <label>Yes</label>
+            </div>
+            <div>
+                <input wire:model="form.is_main_address" type="radio" value="0"/>
+                <label>No</label>
+            </div>
         </div>
 
         <div class="flex justify-center items-center gap-5 mt-4"> 
-            <div>
-                <x-button type="submit" class="hover:bg-red-400 hover:text-purple-100 text-XL font-semibold text-white px-4 py-2 bg-red-500">
+            <div wire:key="1">
+                <x-button class="hover:bg-red-400 hover:text-purple-100 text-XL font-semibold text-white px-4 py-2 bg-red-500">
                     {{ __('Save') }}
                 </x-button>
             </div>
-            <div>
-                <x-button class="hover:bg-red-400 hover:text-purple-100 text-XL font-semibold text-white px-4 py-2 bg-red-500" type="button" wire:click="closeCreateModal()">
+            <div wire:key="2">
+                <x-button wire:click.prevent="closeCreateModal()" class="hover:bg-red-400 hover:text-purple-100 text-XL font-semibold text-white px-4 py-2 bg-red-500">
                     {{ __('Close') }}
                 </x-button>
             </div>
