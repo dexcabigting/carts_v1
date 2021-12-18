4802cb59c5da4583c9e8418cb3dce4a18bcf7b0d
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 19:45:44 +0800

    Created livewire component

---
+++ app/Http/Livewire/Dropdown/DropdownName.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Dropdown;
+
+use Livewire\Component;
+
+class DropdownName extends Component
+{
+    public function render()
+    {
+        return view('livewire.dropdown.dropdown-name');
+    }
+}
