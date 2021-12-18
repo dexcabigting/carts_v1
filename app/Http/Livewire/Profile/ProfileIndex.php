b39f741d1e8f55a7a33ffefcddd4e424fc6da125
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 18:28:16 +0800

    Added new line

--- app/Http/Livewire/Profile/ProfileIndex.php
+++ app/Http/Livewire/Profile/ProfileIndex.php
@@ -9,6 +9,7 @@ class ProfileIndex extends Component
     public function render()
     {
         $layout = (auth()->user()->role_id == 1) ? 'layouts.app' : 'layouts.app-user';
+        
         return view('livewire.profile.profile-index')->layout($layout);
     }
 }
