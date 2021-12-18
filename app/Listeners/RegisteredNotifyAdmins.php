c496571b387bc3774fdb9be41b4831a47f008619
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 15:22:02 +0800

    Listener now calls RegisteredNotifyAdmins

--- app/Listeners/RegisteredNotifyAdmins.php
+++ app/Listeners/RegisteredNotifyAdmins.php
@@ -6,6 +6,10 @@ use App\Events\Registered;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\RegisteredNotification;
+
 class RegisteredNotifyAdmins
 {
     /**
@@ -27,5 +31,8 @@ class RegisteredNotifyAdmins
     public function handle(Registered $event)
     {
         //
+        $admins = User::where('role_id', 1)->get();
+
+        Notification::send($admins, new RegisteredNotification($event->user));
     }
 }
