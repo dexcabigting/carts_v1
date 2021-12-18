9ea34cc333dcfeb43d8d22a18b009d3b9afa5977
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 18:56:27 +0800

    Created livewire component

---
+++ app/Http/Livewire/Profile/ProfileIndexCredentials.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Profile;
+
+use Livewire\Component;
+
+class ProfileIndexCredentials extends Component
+{
+    public function render()
+    {
+        return view('livewire.profile.profile-index-credentials');
+    }
+}
