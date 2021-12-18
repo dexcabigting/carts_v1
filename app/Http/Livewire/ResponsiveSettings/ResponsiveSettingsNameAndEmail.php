86b010da04b69e7099d3c70a703cd71ddcc6b1e0
dexcabigting <dexcabigting1234@gmail.com>
2021-12-18 19:59:41 +0800

    Created livewire component

---
+++ app/Http/Livewire/ResponsiveSettings/ResponsiveSettingsNameAndEmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\ResponsiveSettings;
+
+use Livewire\Component;
+
+class ResponsiveSettingsNameAndEmail extends Component
+{
+    public function render()
+    {
+        return view('livewire.responsive-settings.responsive-settings-name-and-email');
+    }
+}
