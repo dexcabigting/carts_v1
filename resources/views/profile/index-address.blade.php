715109e2a63beab19e36fd9f846dfb71bd74cb42
dexcabigting <dexcabigting1234@gmail.com>
2021-12-19 13:20:09 +0800

    Restructured index-address view

--- resources/views/profile/index-address.blade.php
+++ resources/views/profile/index-address.blade.php
@@ -19,7 +19,7 @@
         <div class="text-sm font-medium text-gray-900">
             <select wire:model="selectedAddress">
                 @foreach($userAddresses as $index => $address)
-                    <option value="{{ $address }}">
+                    <option wire:key="{{ $loop->index }}-address" value="{{ $address }}">
                         Address {{ $index + 1 }}
                     </option>
                 @endforeach
@@ -28,7 +28,7 @@
     </div>         
 
      <div class="mt-4">
-        @if($userAddress->is_main_address == 1)
+        @if($isDefaultAddress)
             <x-input class="block mt-1 w-1/4" type="text" value="Default Address" disabled/>
         @else
             <div>
@@ -45,48 +45,69 @@
        
         <div class="mt-4">
             <x-label class="text-xl mb-4 text-white" :value="__('Home Address')" />
-            <x-input class="block mt-1 w-full" type="text" placeholder="ex. Street/Apartment no." />
+            <x-input wire:model.defer="form.home_address" class="block mt-1 w-full" type="text" placeholder="ex. Street/Apartment no." />
         </div>
 
         <!-- Address -->
         <div class="block md:flex fle-row">
             <div class="mt-4 ">
                 <x-label :value="__('Region')" />
-                <select class="rounded-lg lg:pr-16 pr-4 w-full">
-                    <option selected>
-                        Region
-                    </option>
+                <select wire:model="selectedRegion" class="rounded-lg lg:pr-16 pr-4 w-full">
+                    <option value="" selected>Select Region</option>
+                    @foreach($regions as $region)
+                        <option wire:key="{{ $loop->index }}-region" value="{{ $region['region_id'] }}">
+                            {{ $region['name'] }}
+                        </option>
+                    @endforeach
                 </select>
             </div>
 
-            <div class="mt-4 md:ml-4">
-                <x-label :value="__('Province')" />
-                <select class="rounded-lg md:pr-20 w-full md:w-auto">
-                    <option>
-                        <h1 class="px-12">Province</h1>
-                    </option>
+            @if(!empty($selectedRegion))
+                <div class="mt-4 md:ml-4">
+                    <x-label :value="__('Province')" />
+                    <select wire:model="selectedProvince" class="rounded-lg md:pr-20 w-full md:w-auto">
+                        <option value="" selected>Select Province</option>
+                        @foreach($provinces as $province)
+                            <option wire:key="{{ $loop->index }}-province" value="{{ $province['province_id'] }}" @if($province['province_id'] == '') selected="selected" @endif>
+                                {{ $province['name'] }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            @endif
+        </div>
+
+        @if(!empty($selectedProvince))
+            <div class="mt-4">
+                <x-label :value="__('City')" />
+                <select wire:model="selectedCity" class="rounded-lg w-full">
+                    <option value="" selected>Select City</option>
+                    @foreach($cities as $city)
+                        <option wire:key="{{ $loop->index }}-city" value="{{ $city['city_id'] }}">
+                            {{ $city['name'] }}
+                        </option>
+                    @endforeach
                 </select>
             </div>
-        </div>
+        @endif
 
-        
-        <div class="mt-4">
-            <x-label :value="__('City')" />
-            <select class="rounded-lg w-full">
-                <option>
-                    City
-                </option>
-            </select>
-        </div>
-
-        <div class="mt-4">
-            <x-label :value="__('Barangay')" />
-            <select class="rounded-lg w-full" name="barangay" id="barangay">
-                <option>
-                    Barangay
-                </option>
-            </select>
-        </div>                          
+        @if(!empty($selectedCity) && !empty($barangays))
+            <div class="mt-4">
+                <x-label :value="__('Barangay')" />
+                <select wire:model="selectedBarangay" class="rounded-lg w-full">
+                   <option value="" selected>Select Barangay</option>
+                    @foreach($barangays as $barangay)
+                        <option wire:key="{{ $loop->index }}-barangay" value="{{ $barangay['id'] }}">
+                            {{ $barangay['name'] }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+        @elseif(!empty($selectedCity) && empty($barangays))
+            <div class="mt-4">
+                <x-label value="This city has no barangays." />
+            </div>
+        @endif                          
 
         <div class="flex flex-row mt-4 gap-5">
             <x-button class="hover:bg-purple-900 hover:text-purple-100 text-xl font-semibold text-white w-full px-4 py-2 bg-custom-violet my-3">
