c223d16e75e8bbc067bbfcc092bdc01005fc895e
dexcabigting <dexcabigting1234@gmail.com>
2021-12-21 23:01:06 +0800

    Added comment property

--- app/Events/ProductVariantCommentCreated.php
+++ app/Events/ProductVariantCommentCreated.php
@@ -14,14 +14,16 @@ class ProductVariantCommentCreated
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $comment;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($comment)
     {
         //
+        $this->comment = $comment;
     }
 
     /**
