47d18170586cd298a8601616248ea8f1cea828a3
dexcabigting <dexcabigting1234@gmail.com>
2021-12-21 15:24:16 +0800

    Created CartsCommentIndex component

---
+++ app/Http/Livewire/Shop/Carts/CartsCommentIndex.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Livewire\Shop\Carts;
+
+use Livewire\Component;
+
+use App\Models\ProductVariantComment;
+
+class CartsCommentIndex extends Component
+{
+    public $id = null;
+    public $commentId = null;
+    public $userComment = "";
+
+    protected $rules = [
+        'comment' => 'required|string|max:150'
+    ];
+
+    public function mount($id)
+    {
+        $this->id = $id;
+    }
+
+    public function render()
+    {
+        $comments = $this->comments->get()->toArray();
+
+        return view('livewire.shop.carts.carts-comment-index', compact('comments'));
+    }
+
+    public function addComment()
+    {
+        $this->validate();
+
+        ProductVariantComment::create([
+            'user_id' => auth()->user()->id,
+            'comment' => $this->userComment
+        ]);
+
+        session()->flash('success', 'Comment has been successfully added!');
+    }
+
+    public function editComment($id)
+    {
+        $this->validate();
+
+        $this->commentId = $id;
+
+        $comment = $this->comment->findorFail();
+        
+        $comment->update([
+            'comment' => $this->userComment
+        ]);
+
+        session()->flash('success', 'Comment has been successfully updated!');
+    }
+
+    public function deleteComment($id)
+    {
+        $this->commentId = $id;
+
+        $this->comment->delete();
+
+        session()->flash('success', 'Comment has been successfully deleted!');
+    }
+
+    public function getCommentProperty()
+    {
+        return ProductVariantComment::where('id', $this->commentId);
+    }
+
+    public function getCommentsProperty()
+    {
+        return ProductVariantComment::where('product_variant_id', $this->id);
+    }
+}
