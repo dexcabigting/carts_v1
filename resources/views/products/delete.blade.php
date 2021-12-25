cba480d1741a8e58a95b654152fe433a082da9ea
dexcabigting <dexcabigting1234@gmail.com>
2021-12-25 22:35:57 +0800

    Updated error message and removed parentheses on wire:clicks

--- resources/views/products/delete.blade.php
+++ resources/views/products/delete.blade.php
@@ -2,23 +2,39 @@
     <x-success-fail-message />
 
     @if($promptDelete)   
-        <h2 class=" font-semibold text-l text-gray-100 leading-tight mb-4">
-            @if (count($products) == 1)
-                {{ __('Are you sure you want to delete this product?') }}
+        <div>
+            <h1 class="font-semibold text-xl text-gray-100 leading-tight mb-4">
+                WARNING
+            </h1>
+        </div>
+         
+        <div class="font-semibold text-l text-gray-100 leading-tight mb-4">
+            @if(count($products) == 1)
+                <div>
+                    Associated records with this product will be affected.
+                </div>
+                <div>
+                    Are you sure you want to delete this product?
+                </div>
             @else
-            {{ __('Are you sure you want to delete these products?') }}
+                <div>
+                    Associated records with these products will be affected.
+                </div>
+                <div>
+                    Are you sure you want to delete these products?
+                </div>
             @endif 
-        </h2>
+        </div>
     
         <div class="flex justify-center items-center mt-4 gap-5 ">
             <div class="bg-custom-violet text-xl text-white">
-                <x-button class="font-bold px-6 py-2" type="button" wire:click.prevent="deleteProducts()">
+                <x-button class="font-bold px-6 py-2" type="button" wire:click.prevent="deleteProducts">
                     {{ __('Yes') }}
                 </x-button>
             </div>
             
             <div class="bg-red-500 text-xl text-white">
-                <x-button class="font-bold px-3 py-2" type="button" wire:click.prevent="closeDeleteModal()">
+                <x-button class="font-bold px-3 py-2" type="button" wire:click.prevent="closeDeleteModal">
                     {{ __('Cancel') }}
                 </x-button>
             </div>  
@@ -26,11 +42,10 @@
     @elseif($promptDeleted)
         <div class="flex mt-4">
             <div>
-                <x-button type="button" wire:click.prevent="closeDeleteModal()">
+                <x-button type="button" wire:click.prevent="closeDeleteModal">
                     {{ __('Close') }}
                 </x-button>
             </div>
         </div>
     @endif
 </div>
-
