716e99a8e0b1ffc08e456ad192d5f9cc065b8c8d
dexcabigting <dexcabigting1234@gmail.com>
2021-12-25 16:15:13 +0800

    Listener now fires ProductCreatedNotification

--- app/Listeners/ProductCreatedNotifyUsers.php
+++ app/Listeners/ProductCreatedNotifyUsers.php
@@ -6,6 +6,11 @@ use App\Events\ProductCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ProductCreatedNotification;
+
+use App\Models\User;
+
 class ProductCreatedNotifyUsers
 {
     /**
@@ -27,5 +32,8 @@ class ProductCreatedNotifyUsers
     public function handle(ProductCreated $event)
     {
         //
+        $users = User::where('role_id', 2)->get();
+
+        Notification::send($users, new ProductCreatedNotification($event->product));
     }
 }
