d237937032a3d6f8f79d1f84585fc81420e9877f
dexcabigting <dexcabigting1234@gmail.com>
2021-12-26 21:18:23 +0800

    Updated model

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CartItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'cart_id',
         'size',
         'surname',
-        'jersey_number',
+        'jersey_number'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
     ];
 
     public function cart()
