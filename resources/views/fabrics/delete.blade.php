febea9e21ed580e779d24954ce2fdc6a2e72991c
dexcabigting <dexcabigting1234@gmail.com>
2021-12-26 20:50:45 +0800

    Refactored view

--- resources/views/fabrics/delete.blade.php
+++ resources/views/fabrics/delete.blade.php
@@ -2,23 +2,39 @@
     <x-success-fail-message />
 
     @if($promptDelete)   
-        <h2 class="font-semibold text-l text-gray-100 leading-tight mb-4">
+        <div>
+            <h1 class="font-semibold text-xl text-gray-100 leading-tight mb-4">
+                WARNING
+            </h1>
+        </div>
+         
+        <div class="font-semibold text-l text-gray-100 leading-tight mb-4">
             @if(count($fabrics) == 1)
-                {{ __('Are you sure you want to delete this fabric?') }}
+                <div>
+                    Associated records with this fabric will be affected.
+                </div>
+                <div>
+                    Are you sure you want to delete this fabric?
+                </div>
             @else
-                {{ __('Are you sure you want to delete these fabrics?') }}
+                <div>
+                    Associated records with these fabrics will be affected.
+                </div>
+                <div>
+                    Are you sure you want to delete these fabrics?
+                </div>
             @endif 
-        </h2>
+        </div>
     
         <div class="flex mt-4 gap-5 justify-center">
             <div>
-                <x-button class="bg-custom-violet text-gray-100 text-xl font-bold px-8 py-2" type="button" wire:click.prevent="deleteFabrics()">
+                <x-button type="button" wire:click.prevent="deleteFabrics" class="bg-custom-violet text-gray-100 text-xl font-bold px-8 py-2">
                     {{ __('Yes') }}
                 </x-button>
             </div>
             
             <div>
-                <x-button class="bg-red-500 text-gray-100 text-xl font-bold px-4 py-2" type="button" wire:click.prevent="closeDeleteModal()">
+                <x-button type="button" wire:click.prevent="closeDeleteModal" class="bg-red-500 text-gray-100 text-xl font-bold px-4 py-2">
                     {{ __('Cancel') }}
                 </x-button>
             </div>  
@@ -26,7 +42,7 @@
     @elseif($promptDeleted)
         <div class="flex mt-4 justify-center">
             <div>
-                <x-button type="button" wire:click.prevent="closeDeleteModal()">
+                <x-button type="button" wire:click.prevent="closeDeleteModal">
                     {{ __('Close') }}
                 </x-button>
             </div>
