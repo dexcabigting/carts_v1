0b08ef006989dc9e750c4af913d74d90c32b49ef
dexcabigting <dexcabigting1234@gmail.com>
2021-12-27 11:33:34 +0800

    Updated model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,16 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OrderItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'order_variant_id',
         'size',
         'surname',
         'jersey_number',
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
     ];
 
     public function order_variant()
