718efc3570698d702f60a5bac2e2bfd1162730c4
dexcabigting <dexcabigting1234@gmail.com>
2021-12-27 17:08:46 +0800

    Updated form.name validation rule

--- app/Http/Livewire/Users/UsersCreate.php
+++ app/Http/Livewire/Users/UsersCreate.php
@@ -54,7 +54,7 @@ class UsersCreate extends Component
         $service = app()->make(PhoneNumberLookupService::class);
         
         return [
-            'form.name' => ['required', 'string', 'max:50', 'regex:/^[A-Za-z][A-Za-z\s]*$/'],
+            'form.name' => ['required', 'string', 'max:50', 'regex:/^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/'],
             'form.email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'form.phone' => ['required', 'string', 'min:11', 'max:12', 'unique:users,phone', new PhoneNumber($service)],
             'form.password' => ['required', 'confirmed', Rules\Password::defaults()],
