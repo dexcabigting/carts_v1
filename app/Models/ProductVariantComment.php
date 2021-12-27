2709bb7ffc119b6f05a9a3019975d1383eaa8446
dexcabigting <dexcabigting1234@gmail.com>
2021-12-27 11:38:51 +0800

    Updated model

--- app/Models/ProductVariantComment.php
+++ app/Models/ProductVariantComment.php
@@ -4,15 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductVariantComment extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'product_variant_id',
         'user_id',
         'comment'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
     ];
 
     public function product_variant()
