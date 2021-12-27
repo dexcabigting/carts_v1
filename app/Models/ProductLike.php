58cd7192a260d35a7949caaf7eeb9ca6c5a7ae16
dexcabigting <dexcabigting1234@gmail.com>
2021-12-27 11:16:33 +0800

    Used SoftDeletes trait

--- app/Models/ProductLike.php
+++ app/Models/ProductLike.php
@@ -4,14 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductLike extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
         'product_id'
+    ];
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
     ];
 
     public function user()
