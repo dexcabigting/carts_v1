ce6624681baab43cad813dee9e6c0da3b85e73e1
dexcabigting <dexcabigting1234@gmail.com>
2022-01-13 16:26:41 +0800

    Updated CartsIndex.php

--- app/Http/Livewire/Shop/Carts/CartsIndex.php
+++ app/Http/Livewire/Shop/Carts/CartsIndex.php
@@ -6,12 +6,15 @@ use Livewire\Component;
 use Livewire\WithPagination;
 
 use App\Models\Cart;
+use App\Models\ProductStock;
 
 class CartsIndex extends Component
 {
     use WithPagination;
 
     public $checkedCarts;
+    public $removedKeys;
+    public $withStocks;
     public $cartId;
     public $cartEditModal = false;
     public $cartDeleteModal = false;
@@ -28,13 +31,14 @@ class CartsIndex extends Component
     public function mount()
     {
         $this->checkedCarts = [];
+        $this->removedKeys = [];
+        $this->withStocks = [];
         $this->cartId = [];
     }
 
     public function render()
     {
         $userCarts = $this->user_carts->paginate(6);
-        // dd(Cart::where('user_id', auth()->id())->get());
 
         return view('livewire.shop.carts.carts-index', compact('userCarts'))->layout('layouts.app-user');
     }
@@ -45,15 +49,46 @@ class CartsIndex extends Component
             ->withCount('cart_items');
     }
 
+    public function outOfStock(Cart $id): bool
+    {
+        $sizes = ProductStock::where('product_variant_id', $id->product_variant_id)->first();
+
+        $cartItems = $id->cart_items()->get()->toArray();
+
+        $originalStocks = $sizes->sizes->toArray();
+
+        $variantStocks = array_count_values(array_column($cartItems, 'size'));
+
+        foreach($variantStocks as $size => $count) {
+            $originalCountSize = $originalStocks[$size];
+
+            if($originalCountSize == 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     public function updatedSelectAll($value)
     {
-        if ($value) {
+        if($value) {
             $this->checkedCarts = Cart::where('user_id', auth()->id())
                 ->pluck('id')
                 ->map(fn ($item) => (string) $item)
                 ->flip()
                 ->map(fn ($item) => true)
                 ->toArray();
+
+            foreach($this->checked_keys as $checked_key) {
+                $checkedCart = Cart::findOrFail($checked_key);
+                if($this->outOfStock($checkedCart)) {
+                    array_push($this->removedKeys, $checked_key);
+                    unset($this->checkedCarts[$checked_key]);    
+                } 
+            }
+
+            $this->withStocks = array_values(array_diff($this->checked_keys, $this->removedKeys));
         } else {
             $this->checkedCarts = [];
         }
@@ -63,6 +98,16 @@ class CartsIndex extends Component
     {
         return array_keys($this->checkedCarts);
     }
+
+    // public function getWithStockKeysProperty()
+    // {
+    //     $arr = $this->checked_keys;
+    //     foreach($this->removedKeys as $remove){
+    //         unset($arr[$remove]);
+    //     }
+
+    //     return $arr;
+    // }
 
     public function updatedCheckedCarts()
     {
